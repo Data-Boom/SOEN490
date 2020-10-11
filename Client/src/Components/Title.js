7d2144e45e87db62bdbea4118093927abe82f99b
lposo <lposo26@users.noreply.github.com>
2020-10-11 16:46:01 -0400

    Temporary Header
    Added blue header to app.js. TEsting out components.

---
+++ Client/src/Components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="Title">
+            <header>
+                <h1>Detonation Database</h1>
+            </header>
+        </div>
+    )
+}
