c974c3055a3106116b95b2a55f0c7cabd696e504
lposo <lposo26@users.noreply.github.com>
2020-10-15 18:09:17 -0400

    Changed fonts, added new splash.

--- Client/src/Components/Splash.js
+++ Client/src/Components/Splash.js
@@ -4,10 +4,7 @@ import './Splash.css';
 function Splash(){
     return(
         <div className = "Splash">
-            <img src={require('./img_snow_wide.jpg')} width = "100%" />
-            <h1>Welcome to the</h1>
-            <h2>NEW</h2>
-            <h3>Detonation Database</h3>
+            <img src={require('./splashimage.png')} width = "100%" />
         </div>
     )
 }
