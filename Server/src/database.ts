84e7dee61b87586bda5b05372d834c4a50cf74b2
Gurinder Dheer <>
2020-10-14 22:27:55 -0400

    [#49] Verify TypeORM database calling functions

--- Server/src/database.ts
+++ Server/src/database.ts
@@ -5,25 +5,37 @@ export const connectDB = async () => {
   await createConnection().then( async connection => {
   console.log("Connection was made.")
 
+  //Methods for Entity Manager: https://github.com/typeorm/typeorm/blob/master/docs/entity-manager-api.md
+  // This is using connection.manager.{methodName}
+
   /**
-   * Testing creating an Animerank Table
+   * Testing creating an Animerank elements
    */
   let animeranker = new AnimeRank();
+  animeranker.id = 1;
   animeranker.title = "Attack on Titan";
-  animeranker.rank = 1;
+  animeranker.rank = 1314;
   await connection.manager.save(animeranker);
   console.log("First animerank saved.")
 
   let animeranker2 = new AnimeRank();
+  animeranker2.id = 2;
   animeranker2.title = "One Punch Man"
-  animeranker2.rank = 22;
+  animeranker2.rank = 22214;
   await connection.manager.save(animeranker2);
   console.log("Second animerank saved.")
 
   let animeranker3 = new AnimeRank();
+  animeranker3.id = 3;
   animeranker3.title = "Promised Neverland"
-  animeranker3.rank = 3;
+  animeranker3.rank = 2143;
   await connection.manager.save(animeranker3);
-  console.log(" animerank saved.")
+  console.log(" animerank saved.");
+
+  /**
+   * Testing finding an element
+   */
+   let savedAnime = await connection.manager.find(AnimeRank, { title: "Promised Neverland"});
+   console.log(savedAnime);
 
   })};
