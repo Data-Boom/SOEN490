16f5d924fb63297345e9b12add10aa65e2875f9e
dmitry <ds1618033@gmail.com>
2020-10-30 14:48:30 -0400

    wip pre something

---
+++ Client/src/Views/HomeView.js
@@ -0,0 +1,36 @@
+import './App.css'
+
+import React, { useEffect, useState } from "react"
+
+import Blurb from '../Components/Blurb/Blurb'
+import { Button } from '@material-ui/core'
+import DataCell from '../Components/DataCell/DataCell'
+import Splash from '../Components/Splash/Splash'
+import Title from '../Components/Title/Title'
+import fetch from "node-fetch"
+
+export default function HomeView() {
+
+  const [apiResponse, setApiResponse] = useState('')
+
+  function callAPI() {
+    fetch('http://localhost:4000/note')
+      .then(res => res.json())
+      .then(data => setApiResponse(data))
+  }
+
+  useEffect(() => {
+    callAPI()
+  }, [])
+
+  return (
+    <div className="App">
+      <Button></Button>
+      <Title></Title>
+      <Splash />
+      <Blurb />
+      <DataCell />
+      <p className="App-intro">{apiResponse}</p>
+    </div>
+  )
+}
