3f06acfa1ae6f542dd681bbcd48ef2368c60547b
dmitry <ds1618033@gmail.com>
2020-10-30 17:29:56 -0400

    cleanup

--- Client/src/Components/DataCell/DataCell.js
+++ Client/src/Components/DataCell/DataCell.js
@@ -3,12 +3,16 @@ import Button from '@material-ui/core/Button'
 import React from 'react'
 import uploadimage from './uploadimage.png'
 
-const DataCell = () => (
-  <Box display="flex" marginLeft={"25%"} flexDirection="column" justify="center" alignItems="center" border={30} borderColor={"#2e3b52"} width={720} height={300} padding={10}>
-    <img src={uploadimage} />
-    <p>Choose a File or Drag/Drop here</p>
-    <Button onClick={DataCell.callAPI} variant="contained" t={0.5}>Browse</Button>
-  </Box>
-)
+export default function DataCell() {
+  const callAPI = () => {
+    fetch("http://localhost:4000/dataupload")
+  }
 
-export default DataCell
+  return (
+    <Box display="flex" marginLeft={"25%"} flexDirection="column" justify="center" alignItems="center" border={30} borderColor={"#2e3b52"} width={720} height={300} padding={10}>
+      <img src={uploadimage} />
+      <p>Choose a File or Drag/Drop here</p>
+      <Button onClick={callAPI} variant="contained" t={0.5}>Browse</Button>
+    </Box>
+  )
+}
