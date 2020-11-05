d41a5d71eda6406a3f94abbe2cda3a816275c745
Jessica Allaire <jessica.allaire.96@hotmail.com>
2020-11-04 21:36:03 -0500

    Got the React.useRef to work, can now work with functional.
    Changed the this.refs.canvas to ref.current from the new React.useRef(), this allows for functions. Also, the use of useEffect allows for a one time call when mounted.

--- Client/src/Views/GraphView.js
+++ Client/src/Views/GraphView.js
@@ -3,7 +3,7 @@ import React from "react"
 import Graph from '../Components/Graph/Graph'
 
 export default function GraphView() {
-
+  //<Button onClick={handleRequest}>Why am I here?</Button>
   // const [count, setCount] = useState(0)
   const handleRequest = () => {
     alert('clicked')
