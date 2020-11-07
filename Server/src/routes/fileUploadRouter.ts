e95e07c387a01f296448499cf291ff584e682006
Gurinder Dheer <gindulink@gmail.com>
2020-11-07 14:55:51 -0500

    [#71] Fix comments

--- Server/src/routes/fileUploadRouter.ts
+++ Server/src/routes/fileUploadRouter.ts
@@ -1,23 +1,23 @@
 var express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: 'tmp/csv/' });
+const upload = multer({ dest: 'tmp/json/' });
 const fileUploadController = require('../controllers/fileUploadController');
-
-
-/**
- * This route will accept a CSV file and forward to the router. It is first processed by multer middleware,
- * and the file is stored in a temporary directory called tmp/csv. This route is referred for processing by 
- * the service.
- */
-router.post('/dataupload', upload.single('jsonFile'), fileUploadController.createRequest);
 
 /**
  * This file contains the routes for a call to dataupload. Only a post method will remain, get is left for
  * testing purposes. If an API call is made to /dataupload then the request is routed to the fileUploadController
  * to continue processing of the request.
  */
-//router.post('/dataupload', upload.single('csvFile'), fileUploadController.createRequest);
+
+
+/**
+ * This route will accept a JSON file and forward to the router. It is first processed by multer middleware,
+ * and the file is stored in a temporary directory called tmp/json. This route is referred for processing by 
+ * the service.
+ */
+router.post('/dataupload', upload.single('jsonFile'), fileUploadController.createRequest);
+
 
 //File Upload Get Router
 router.get('/dataupload', function (request, response) {
