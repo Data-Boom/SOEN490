f2cfdf4cdf761b41170c9ad38826b770a768335e
Gurinder Dheer <gindulink@gmail.com>
2020-11-07 12:51:55 -0500

    [#71] Temp changes for JSON support

--- Server/src/routes/fileUploadRouter.ts
+++ Server/src/routes/fileUploadRouter.ts
@@ -10,7 +10,7 @@ const fileUploadController = require('../controllers/fileUploadController');
  * and the file is stored in a temporary directory called tmp/csv. This route is referred for processing by 
  * the service.
  */
-router.post('/dataupload', upload.single('csvFile'), fileUploadController.createRequest);
+router.post('/dataupload', upload.single('jsonFile'), fileUploadController.createRequest);
 
 /**
  * This file contains the routes for a call to dataupload. Only a post method will remain, get is left for
@@ -20,13 +20,13 @@ router.post('/dataupload', upload.single('csvFile'), fileUploadController.create
 //router.post('/dataupload', upload.single('csvFile'), fileUploadController.createRequest);
 
 //File Upload Get Router
-router.get('/dataupload', function(request, response)  {
-    // Call the controller for get
-    response.status(200).json("upload endpoint is reached!");
+router.get('/dataupload', function (request, response) {
+  // Call the controller for get
+  response.status(200).json("upload endpoint is reached!");
 });
 
-router.get('/note', function(req,res) {
-    res.status(200).json("L, did you know Shinigami love apples?");
-  });
+router.get('/note', function (req, res) {
+  res.status(200).json("L, did you know Shinigami love apples?");
+});
 
 module.exports = router;
