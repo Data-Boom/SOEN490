37e302013e19662914f0e50f25ceaa51756c21c2
Rahimuz Zaman <rahimuz_z@yahoo.ca>
2020-11-08 10:36:27 -0500

    [51] added MUI result section

--- Client/src/Components/Search/search2.js
+++ Client/src/Components/Search/search2.js
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { Container,Grid,InputLabel,TextField, FormControl,Select } from '@material-ui/core';
+import { Container,Grid,InputLabel,TextField, FormControl,Select, RadioGroup, FormControlLabel, Radio, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -47,6 +47,17 @@ export default function App(){
       [n2]:event.target.value,
     });
   }
+  function createData(title, oxidizer, category, subcategory, fuel, diluent, author, year, outputFormat) {
+    return { title, oxidizer, category, subcategory, fuel, diluent, author, year, outputFormat };
+  }
+  
+  const rows = [
+    createData(),
+    createData(),
+    createData(),
+    createData(),
+    createData(),
+  ];
 
 
   return (
@@ -64,9 +75,9 @@ export default function App(){
       </AppBar>
 
       <Container >
-        <h1>
+        <Typography variant={'h3'}>
           Search
-        </h1>  
+        </Typography>  
 
         <div >{/**for columns */}
         
@@ -76,7 +87,7 @@ export default function App(){
 
                   <Grid item xs={2}>
                     <form >      
-                    <TextField id="outlined-basic" label="Oxidizer" variant="outlined" />
+                    <TextField id="outlined-basic" label="Oxidizer" variant="outlined"  />
                     </form>
                   </Grid>
 
@@ -109,7 +120,7 @@ export default function App(){
 
                   <Grid item xs={1}>
                   <FormControl variant="outlined" >
-                      <InputLabel htmlFor="outlined-outFormat-native-simple">Categories</InputLabel>
+                      <InputLabel htmlFor="outlined-outFormat-native-simple" >Categories</InputLabel>
                       <Select
                         native
                         value={state.categories}
@@ -138,13 +149,13 @@ export default function App(){
 
                   <Grid item xs={2}>
                     <form >      
-                    <TextField id="outlined-basic" label="Subcategories" variant="outlined" />
+                    <TextField id="outlined-basic" label="Subcategories" variant="outlined" size={"small"}/>
                     </form>
                   </Grid>
 
                   <Grid item xs={2}>
                     <form >      
-                    <TextField id="outlined-basic" label="Fuel" variant="outlined" />
+                    <TextField id="outlined-basic" label="Fuel" variant="outlined" size={"small"} />
                     </form>
                   </Grid>
 
@@ -155,21 +166,87 @@ export default function App(){
 
                       <Grid item xs={2}>
                         <form >      
-                        <TextField id="outlined-basic" label="Author" variant="outlined" />
+                        <TextField id="outlined-basic" label="Author" variant="outlined" size={"small"}/>
                         </form>
                       </Grid>
 
                       <Grid item xs={2}>
                         <form >      
-                        <TextField id="outlined-basic" label="Diluent" variant="outlined" />
+                        <TextField id="outlined-basic" label="Diluent" variant="outlined" size={"small"}/>
                         </form>
                       </Grid>
 
                     </Grid>
                 </Grid>
-
                 
             </Grid>
+
+            <Typography variant={'h6'} display={'inline'}> Case Sensitive? </Typography>
+
+            <FormControl component="fieldset">
+              <RadioGroup row aria-label="position" name="position" defaultValue="top">
+                <FormControlLabel
+                  value="start"
+                  control={<Radio color="primary" />}
+                  label="Yes"
+                  labelPlacement="start"
+                />
+                <FormControlLabel
+                  value="end"
+                  control={<Radio color="primary" />}
+                  label="No"
+                  labelPlacement="start"
+                />
+              </RadioGroup>
+            </FormControl>
+            
+            <div>
+             <Typography variant={'h3'}>
+                  Results
+             </Typography>
+
+             {/* function createData(title, oxidizer, category, subcategory, fuel, diluent, author, year, outputFormat) {
+                return { title, oxidizer, category, subcategory, fuel, diluent, author, year, outputFormat };
+              } */}
+
+             <TableContainer >
+              <Table className={classes.table} aria-label="simple table" size={"small"}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Title</TableCell>
+                    <TableCell align="right">Oxidizer</TableCell>
+                    <TableCell align="right">Category</TableCell>
+                    <TableCell align="right">Subcategory</TableCell>
+                    <TableCell align="right">Fuel</TableCell>
+                    <TableCell align="right">Diluent</TableCell>
+                    <TableCell align="right">Author</TableCell>
+                    <TableCell align="right">Year</TableCell>
+                    <TableCell align="right">Output Format</TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.title}</TableCell>
+                      <TableCell align="right">{row.oxidizer}</TableCell>
+                      <TableCell align="right">{row.category}</TableCell>
+                      <TableCell align="right">{row.subcategory}</TableCell>
+                      <TableCell align="right">{row.fuel}</TableCell>
+                      <TableCell align="right">{row.diluent}</TableCell>
+                      <TableCell align="right">{row.author}</TableCell>
+                      <TableCell align="right">{row.year}</TableCell>
+                      <TableCell align="right">{row.outputFormat}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                
+              </Table>
+            </TableContainer>
+            </div>
         </div>
       </Container>
       
