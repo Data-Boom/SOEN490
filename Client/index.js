9c6e72fdb7b196ae267ac1be6e302610acd19d41
vatika <vatikaprasad@gmail.com>
2020-11-07 22:16:19 -0500

    Correction: selenium setup on Client directory

---
+++ Client/index.js
@@ -0,0 +1,8 @@
+const {Builder, By, Key, util}= require("selenium-webdriver");
+async function test()
+{
+let driver = await new Builder().forBrowser("firefox").build();
+await driver.get("localhost:4500");
+}
+
+test();
