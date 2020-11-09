6dc0a9c9f4ebc6573c011e153285a68e37ba6b6d
Gurinder Dheer <gindulink@gmail.com>
2020-11-08 19:52:22 -0500

    [53] Add router, test branch commit

---
+++ Server/src/routes/queryDataRouter.ts
@@ -0,0 +1,13 @@
+var express = require('express');
+const getDataRouter = require('express').Router();
+const getDataController = require('../controllers/getDataController');
+
+/**
+ * This file contains the routes for a call to query or obtain a dataset. 
+ * If an API call is made to /getData then the request is routed to the getDataController
+ * to continue processing of the request.
+ */
+getDataRouter.get('/getData', getDataController.createRequestForData);
+
+
+module.exports = getDataRouter;
