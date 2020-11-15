f822ad20c60ce1f9612f8107bed618ba392dd576
dmitry <ds1618033@gmail.com>
2020-11-15 11:20:39 -0500

    indent cleanup

--- Client/src/Views/FileUploadView.js
+++ Client/src/Views/FileUploadView.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 const FileUploadView = () => {
 
-    return (
-        <> <h2> File Upload Page </h2>
-            <DataCell />
-        </>
-    )
+  return (
+    <> <h2> File Upload Page </h2>
+      <DataCell />
+    </>
+  )
 }
 
 export default FileUploadView;
