440a51835389c296645f3f1d7a7651ba8f8bbf2c
Arrow105 <sean.how0@gmail.com>
2020-11-29 22:25:21 -0500

    [#53] Added search queries for any of the following: data set ID (already present), material (accepts composition or material details), author (needs first and last name), publication year, category id, and category id+subcategory id. Each query returns a 2D array to the front end which has everything that Additionally cleared out some now unused /routes, /models, and /controllers files from prior work on this issue

--- Server/src/services/getDataService.ts
+++ Server/src/services/getDataService.ts
@@ -4,12 +4,28 @@ const obtainDataModel = require('../models/SelectQueryDatabase');
 const retrieveData = async () => {
 
     let datasetReceived = 1;
-    let dataset2 = 2;
-    let setOfDataPoints = [];
+    //let materialReceived = "Carbon, Graphite, Pressed Graphite";
+    let materialReceived = "C";
+    let firstNameReceived = "R.";
+    let lastNameReceived = "Ackbar";
+    let yearReceived = 1997;
+    let categoryReceived = 2;
+    let subcategoryReceived = 2;
+    let setOfData = [];
 
-    setOfDataPoints = await obtainDataModel.getDataSet(datasetReceived, dataset2);
+    //setOfData = await obtainDataModel.getDataFromDataset(datasetReceived);
+    //setOfData = await obtainDataModel.getDataFromMaterial(materialReceived);
+    setOfData = await obtainDataModel.getDataFromAuthor(firstNameReceived, lastNameReceived);
+    //setOfData = await obtainDataModel.getDataFromCategory(categoryReceived);
+    //setOfData = await obtainDataModel.getDataFromSubcategory(categoryReceived, subcategoryReceived);
+    //setOfData = await obtainDataModel.getDataFromYear(yearReceived);
 
-    return "Success";
+    // Creates a 2D array of objects. Entries in order are: 
+    // Publication info, author names, data set info, materials,
+    // data points themselves, and data point comments
+    // For example to get the compostion of a second material:
+    // setOfData[3][1].composition_name
+    return setOfData;
 }
 
 module.exports = { retrieveData };
