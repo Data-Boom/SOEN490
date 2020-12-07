07cd994096616ce850ce923b3d3e1984d3481b82
Mehrdad Ahmadi <mehrdad.ahmadi@bell.ca>
2020-12-07 00:07:48 -0500

    Refactor ui tests

--- Server/src/tests/unit/fileUploadController.test.js
+++ Server/src/tests/unit/fileUploadController.test.js
@@ -4,20 +4,20 @@ const dataUploadModel = require("../../models/DataUploadModel");
 dataUploadModel.insertReferenceType = jest.fn();
 
 //check if there is createRequest function
-describe("fileUploadController.createRequest function", ()=>{
-it("should have createRequest function",()=>{
-    expect(typeof fileUploadController.createRequest).toBe("function");
-})
+describe("fileUploadController.createRequest function", () => {
+    it("should have createRequest function", () => {
+        expect(typeof fileUploadController.createRequest).toBe("function");
+    })
 
-it("should have callFileUploadService function",()=>{
-    expect(typeof fileUploadController.callFileUploadService).toBe("function");
-})
+    it("should have callFileUploadService function", () => {
+        expect(typeof fileUploadController.callFileUploadService).toBe("function");
+    })
 
-it("should call createRequest function",()=>{
-    
-    fileUploadController.createRequest();
-    expect(dataUploadModel.insertReferenceType).toBeCalled();
-})
+    it("should call createRequest function", () => {
+
+        fileUploadController.createRequest();
+        expect(dataUploadModel.insertReferenceType).toBeCalled();
+    })
 
 
 
