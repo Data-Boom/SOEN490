dcda140120b96d9e279a09c58f8216b2cfafc632
laith awad <layth_awad@hotmail.com>
2020-12-14 03:04:35 -0500

    Added graph view to table
    Added a sample png graph image and sample data to view on the saved datasets/graphs table, with a condition statement to show the image of the graph or the details of the datasets.

--- Client/src/Views/ProfileView.tsx
+++ Client/src/Views/ProfileView.tsx
@@ -14,7 +14,8 @@ import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { ICompleteDatasetEntity, exampleDatasets } from '../Models/Datasets/ICompleteDatasetEntity'
-
+import { graphRoute } from '../Consts/Routes';
+import DataboomTestGraph from './DataboomTestGraph.png';
 
 let foundDatasets = exampleDatasets;
 
@@ -28,22 +29,48 @@ const useRowStyles = makeStyles({
 });
 
 
-function createData(title: string, name: string, oxidizer: string, author: string, year: string) {
-    return { title, name, oxidizer, author, year };
+function createData(id: number, title: string, name: string, oxidizer: string, author: string, year: string, graphdatasets, comments: string) {
+    return { id, title, name, oxidizer, author, year, graphdatasets, comments };
 };
 
 const rows = [];
 
 for (let i = 0; i < exampleDatasets.length; i++) {
-    rows.push(createData('Dataset', exampleDatasets[i].name, exampleDatasets[i].oxidizer, exampleDatasets[i].author,
-        exampleDatasets[i].year))
+    rows.push(createData(exampleDatasets[i].id, 'Dataset', exampleDatasets[i].name, exampleDatasets[i].oxidizer, exampleDatasets[i].author,
+        exampleDatasets[i].year, ['Cell width', 'Cell Height of O2 explosion', 'Critical energy after N2 intake'], 'Here is a sample comment'))
 };
+
+function createGraphData(id: number, title: string, name: string, graphdatasets, comments: string) {
+    return { id, title, name, graphdatasets, comments };
+};
+
+rows.push(createGraphData(1, 'Graph', 'graph name', ['Cell width', 'Cell Height of O2 explosion', 'Critical energy after N2 intake'],
+    'Here is a sample comment'));
+
+// const handleCellClick = (e) => {
+//     console.log(e.target.textContent);
+// }
+const handleCellClick = (id) => {
+
+    return (event) => {
+        console.log('You clicked on a row with id ' + id);
+    }
+    // fetch("../Consts/Routes")
+    //     .then(function (response) {
+    //         console.log("it worked.", response)
+    //         this.props.history.push({
+    //             pathname: graphRoute
+    //         });
+    //     }).catch(function () {
+    //         console.log("error in routing");
+    //     });
+
+}
 
 function Row(props: { row: ReturnType<typeof createData> }) {
     const { row } = props;
     const [open, setOpen] = React.useState(false);
     const classes = useRowStyles();
-
     return (
         <React.Fragment>
             <TableRow className={classes.root}>
@@ -52,7 +79,7 @@ function Row(props: { row: ReturnType<typeof createData> }) {
                         {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                     </IconButton>
                 </TableCell>
-                <TableCell component="th" scope="row">
+                <TableCell component="th" scope="row" onClick={handleCellClick(row.id)} >
                     {row.name}
                 </TableCell>
                 <TableCell align="right">{row.title}</TableCell>
@@ -64,24 +91,47 @@ function Row(props: { row: ReturnType<typeof createData> }) {
                             <Typography variant="h6" gutterBottom component="div">
                                 Details
                 </Typography>
-                            <Table size="small" aria-label="purchases">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Oxidizer</TableCell>
-                                        <TableCell>Author</TableCell>
-                                        <TableCell>Year</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow >
-                                        <TableCell component="th" scope="row">
-                                            {row.oxidizer}
-                                        </TableCell>
-                                        <TableCell>{row.author}</TableCell>
-                                        <TableCell>{row.year}</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
+                            <div>
+                                {row.title === "Dataset" ? (
+                                    <Table size="small" aria-label="purchases">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Oxidizer</TableCell>
+                                                <TableCell>Author</TableCell>
+                                                <TableCell>Year</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            <TableRow >
+                                                <TableCell component="th" scope="row">
+                                                    {row.oxidizer}
+                                                </TableCell>
+                                                <TableCell>{row.author}</TableCell>
+                                                <TableCell>{row.year}</TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                    </Table>
+                                ) : (
+                                        <Table size="small" aria-label="purchases">
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell>Graph image</TableCell>
+                                                    <TableCell>Datasets in graph</TableCell>
+                                                    <TableCell>Comments</TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                <TableRow >
+                                                    <TableCell component="th" scope="row">
+                                                        <img src={DataboomTestGraph} width="300" height="300" />
+                                                    </TableCell>
+                                                    <TableCell>{row.graphdatasets}</TableCell>
+                                                    <TableCell>{row.comments}</TableCell>
+                                                </TableRow>
+                                            </TableBody>
+                                        </Table>
+                                    )}
+                            </div>
                         </Box>
                     </Collapse>
                 </TableCell>
