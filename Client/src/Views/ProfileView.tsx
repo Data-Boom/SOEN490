20425fe2088a1248224d380d1cdb938a4a000f36
laith awad <layth_awad@hotmail.com>
2020-12-13 20:53:08 -0500

    Expandable table
    Added an expandable option to show more details on the dataset selected

--- Client/src/Views/ProfileView.tsx
+++ Client/src/Views/ProfileView.tsx
@@ -1,60 +1,110 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { ICompleteDatasetEntity, exampleDatasets } from '../Models/Datasets/ICompleteDatasetEntity'
 
 
-let selectedDatasets: ICompleteDatasetEntity[] = []
 let foundDatasets = exampleDatasets;
 
 
-
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-        maxWidth: 700,
+const useRowStyles = makeStyles({
+    root: {
+        '& > *': {
+            borderBottom: 'unset',
+        },
     },
 });
 
-let test = exampleDatasets[0].name;
 
-function createData(title: string, name: string) {
-    return { title, name: name };
+function createData(title: string, name: string, oxidizer: string, author: string, year: string) {
+    return { title, name, oxidizer, author, year };
 };
 
 const rows = [];
 
 for (let i = 0; i < exampleDatasets.length; i++) {
-    rows.push(createData('Dataset', exampleDatasets[i].name))
+    rows.push(createData('Dataset', exampleDatasets[i].name, exampleDatasets[i].oxidizer, exampleDatasets[i].author,
+        exampleDatasets[i].year))
 };
 
-export default function ProfileView() {
-    const classes = useStyles();
+function Row(props: { row: ReturnType<typeof createData> }) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
+    const classes = useRowStyles();
 
     return (
+        <React.Fragment>
+            <TableRow className={classes.root}>
+                <TableCell>
+                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell align="right">{row.title}</TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box margin={1}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Details
+                </Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Oxidizer</TableCell>
+                                        <TableCell>Author</TableCell>
+                                        <TableCell>Year</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow >
+                                        <TableCell component="th" scope="row">
+                                            {row.oxidizer}
+                                        </TableCell>
+                                        <TableCell>{row.author}</TableCell>
+                                        <TableCell>{row.year}</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
+
+export default function CollapsibleTable() {
+    return (
         <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
+            <Table aria-label="collapsible table">
                 <TableHead>Favourites
                     <TableRow>
-                        <TableCell>Title</TableCell>
-                        <TableCell align="right">Name</TableCell>
+                        <TableCell />
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Title</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
-                        <TableRow key={row.title}>
-                            <TableCell component="th" scope="row">
-                                {row.title}
-                            </TableCell>
-                            <TableCell align="right">{row.name}</TableCell>
-
-                        </TableRow>
+                        <Row key={row.title} row={row} />
                     ))}
                 </TableBody>
             </Table>
