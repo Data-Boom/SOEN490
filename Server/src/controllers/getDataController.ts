bfcc9264540629d86d7193d3bb025c170224923d
Arrow105 <sean.how0@gmail.com>
2020-12-15 00:06:09 -0500

    [#53] Added support for searching with an array of data set IDs, added detailed comments to getDataService, tweaked getDataController slightly

--- Server/src/controllers/getDataController.ts
+++ Server/src/controllers/getDataController.ts
@@ -1,25 +1,42 @@
+import { Request, Response } from 'express';
 import { retrieveData } from '../services/getDataService';
 import { IDataRequestModel } from "../models/interfaces/DataRequestModelInterface";
 
 export class getDataController {
+    private validatedRequest: IDataRequestModel;
+    constructor() {
+    }
     async createRequestForData(request: Request, response: Response) {
-        let validatedData = this.validateInputData(request, response);
-        try {
-            const retrieveDataObject = new retrieveData();
-            let arrayOfData = await retrieveDataObject.getArrayOfDatasets(validatedData)
-            return response.status(200).send(arrayOfData);
-        } catch (err) {
-            response.status(500).send(err);
+        let validateData = this.validateInputData(request)
+        console.log(validateData)
+        if (!validateData) {
+            response.status(500).send("Invalid search params entered");
+        }
+        else {
+            this.validatedRequest = {} as any;
+            this.validatedRequest.datasetId = request.query.datasetId as any[];
+            this.validatedRequest.material = request.query.material as string[];
+            this.validatedRequest.year = request.query.year as any;
+            this.validatedRequest.firstName = request.query.firstName as string;
+            this.validatedRequest.lastName = request.query.lastName as string;
+            this.validatedRequest.categoryId = request.query.categoryId as any;
+            this.validatedRequest.subcategoryId = request.query.datasetId as any;
+            try {
+                const retrieveDataObject = new retrieveData();
+                let arrayOfData = await retrieveDataObject.getArrayOfDatasets(this.validatedRequest)
+                return response.status(200).send(arrayOfData);
+            } catch (err) {
+                response.status(500).send(err);
+            }
         }
     }
 
-    private validateInputData(request: Request, response: Response) {
-        let validatedRequest: IDataRequestModel;
-        validatedRequest = request.query;
-        if (validatedRequest.datasetId != undefined && validatedRequest.material.length > 0
-            && validatedRequest.year != undefined && validatedRequest.categoryId != undefined
-            && (validatedRequest.firstName != undefined || validatedRequest.lastName != undefined))
-            response.status(500).send("Invalid search params entered");
-        return validatedRequest
+    private validateInputData(request: Request) {
+        if (request.query.hasOwnProperty('datasetId') || request.query.hasOwnProperty('material')
+            || request.query.hasOwnProperty('year') || request.query.hasOwnProperty('categoryId')
+            || (request.query.hasOwnProperty('firstName') && request.query.hasOwnProperty('lastName'))) { return true }
+        else {
+            return false
+        }
     }
 }
--- Server/src/services/getDataService.ts
+++ Server/src/services/getDataService.ts
@@ -9,16 +9,34 @@ export class retrieveData {
         this.dataQuery = new DataQueryModel();
     }
 
+    /**
+     * This is the 'main' method of database searches. It receives all the data and then it does a check if it 
+     * was given an array of data set IDs. If it did not receive an array of data set IDs, 
+     * it will call @getDatasetIdsFromParams to grab an array of data set IDs based on the various search
+     * parameters (materials, authors, category, and/or subcategory). Then with the newly generated array of 
+     * data set IDs, or if such an array was received at the beginning, this method will then call 
+     * @getDataFromDatasetIds for retreving the information of the data sets themselves.
+     * 
+     * @param receivedData 
+     * This param is an object of type IDataRequestModel. 
+     * It contains all the sorted search parameters, that being any combination of the following:
+     * An array of data set IDs: number[]
+     * An array of materials, where each material is either the composition or the material details: string[]
+     * The first name of an author: string
+     * The last name of an author: string 
+     * Publication year: number
+     * A category ID: number
+     * A subcategory ID: number
+     */
     async getArrayOfDatasets(receivedData: IDataRequestModel) {
         let datasetReceived = receivedData.datasetId;
         let materialReceived = receivedData.material;
-        console.log(receivedData.material);
         let firstNameReceived = receivedData.firstName;
         let lastNameReceived = receivedData.lastName;
         let yearReceived = receivedData.year;
         let categoryReceived = receivedData.categoryId;
         let subcategoryReceived = receivedData.subcategoryId;
-        let selectedDatasetIds = [datasetReceived];
+        let selectedDatasetIds = datasetReceived;
         if (datasetReceived == undefined) {
             selectedDatasetIds = await this.getDatasetIdsFromParams(materialReceived, yearReceived, firstNameReceived, lastNameReceived, categoryReceived, subcategoryReceived)
         }
@@ -26,13 +44,45 @@ export class retrieveData {
         return setOfData;
     }
 
+    /**
+     * This method is used to get an array of data set IDs that match all the entered search parameters. 
+     * Notably at the beginning it declares paramsEntered, which is a number value that will keep 
+     * track of how many search parameters were entered, and rawDatasetIds which is an array that will
+     * contain the unsorted data set IDs that are connected to each individual parameter.
+     * 
+     * After this, for each parameter the method will check if the parameter is defined and then 
+     * for each valid value it shall increment paramsEntered, call a query to get a raw data packet 
+     * which contains the data set IDs of this parameter, and then it will feed this raw data packet to 
+     * @createDatasetIdArray to get an array of data set IDs and append this new array to rawDatasetIds.
+     * 
+     * It is worth noting that a array of materials are received, and thus that entire array is looped
+     * through to check all included materials, each check using the process described above. 
+     * For subcategory, that parameter is always paired with a category, and thus when a
+     * category is defined there is a second check to see if a subcategory is included, and will run
+     * a different query to the data base if so.
+     * 
+     * Once all the parameters have been analyzed, the paramsEntered and rawDatasetIds are passed to
+     * the @selectDatasetIds method to generate an array containing all the common data set IDs among
+     * the parameters.
+     * 
+     * @param materialReceived 
+     * An array of materials, where each material is either the composition or the material details: string[]
+     * @param yearReceived 
+     * Publication year: number
+     * @param firstNameReceived 
+     * The first name of an author: string
+     * @param lastNameReceived 
+     * The last name of an author: string 
+     * @param categoryReceived 
+     * A category ID: number
+     * @param subcategoryReceived 
+     * A subcategory ID: number
+     */
     private async getDatasetIdsFromParams(materialReceived: string[], yearReceived: number, firstNameReceived: string, lastNameReceived: string, categoryReceived: number, subcategoryReceived: number) {
         let rawData;
         let paramsEntered = 0;
-        // paramsEntered is used to track how many params were entered
         let rawDatasetIds = [];
 
-        // Check which variables were received and the data sets IDs linked to each variable
         if (materialReceived != undefined) {
             for (let i = 0; i < materialReceived.length; i++) {
                 paramsEntered++
@@ -64,6 +114,12 @@ export class retrieveData {
         return selectedDatasetIds
     }
 
+    /**
+     * This method accepts an array of raw data packets and iterates through the array to take each packet's
+     * data set ID and appends this number to a new array. Once the loop completes, the new array is returned.
+     * @param rawData 
+     * This is an array of raw data packets (aka. JSON objects): any[]
+     */
     private async createDatasetIdArray(rawData: any[]) {
         let datasetIdArray = [];
         for (let index = 0; index < rawData.length; index++) {
@@ -72,9 +128,20 @@ export class retrieveData {
         return datasetIdArray;
     }
 
+    /**
+     * This method takes an array of numbers, specifically data set IDs, iterates through this array, 
+     * and checks each array entry to see if the total occurances of this particular entry matches the 
+     * paramsEntered number. If there is a match, then the ID in question is one that is desired, and
+     * is appended to the selectedDatasetIds array. Once the looping is complete, the 
+     * selectedDatasetIds array is returned.
+     * 
+     * @param paramsEntered 
+     * Number that will be used to determine if a particular data set ID makes the cut: number
+     * @param rawDatasetIds 
+     * An array formed from combining multiple data set IDs arrays together: any[]
+     */
     private async selectDatasetIds(paramsEntered: number, rawDatasetIds: any[]) {
         let selectedDatasetIds = []
-        // Find the intersection of data sets IDs among all the different variables
         let count: number
         for (let i = 0; i < rawDatasetIds.length; i++) {
             count = 1;
@@ -90,6 +157,17 @@ export class retrieveData {
         return selectedDatasetIds;
     }
 
+    /**
+     * This declares an array of IDatasetResponseModel objects called setOfData and each IDatasetResponseModel 
+     * contains: a publication object, an array of author objects, a data set object, an array of material objects, 
+     * an array of data point objects, and a data point comments object.
+     * It then loops through the received array of data set IDs and runs a @getAllData query for each individual 
+     * entry in the array and appends the returned IDatasetResponseModel object to setOfData. Once the loop is 
+     * completed, setOfData is returned. 
+     *  
+     * @param selectedDatasetIds 
+     * This is an array containing the data set IDs that we wish to get the full data set of: any[]
+     */
     private async getDataFromDatasetIds(selectedDatasetIds: any[]) {
         // Query each data set ID to get its information and add to array of data
         let setOfData: Array<IDatasetResponseModel> = [];
