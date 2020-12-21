a2ceac50e325c16355fc04e943b4860a79b38123
Gurinder Dheer <gindulink@gmail.com>
2020-12-20 21:34:25 -0500

    [#145] Add unit test

--- Server/src/tests/services/authenticationService.spec.ts
+++ Server/src/tests/services/authenticationService.spec.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { createConnection, getConnection } from 'typeorm';
+import { createConnection, getConnection, TableForeignKey } from 'typeorm';
 import { AuthenticationService } from '../../services/authenticationService'
 
 
@@ -33,10 +33,17 @@ describe('Authentication tests', () => {
         expect(regexp.test(passwordHash));
     });
 
-
-
-
-
-
-
+    // test('Valid Fetching User Details Request', async () => {
+    //     const expectedResponse = {
+    //         "account_email": "j.com",
+    //         "account_firstName": "g",
+    //         "account_lastName": "d23232323",
+    //         "account_dateOfBirth": "1980-01-01T00:00:00.000Z",
+    //         "account_organizationName": "mugiwara4545"
+    //     }
+    //     let userEmail: string = "j.com";
+    //     let res: any = await authenticationService.loadUserDetails(userEmail);
+    //     expect(res.status).toBeCalledWith(400);
+    //     expect(res.message).toBe(expectedResponse);
+    // });
 })
