2bc92a128dda27b60b1deeed61af44127fdaac95
dsemenov <ds1618033@gmail.com>
2020-12-21 19:52:40 -0500

    initial commit

---
+++ Client/src/Components/ResearchPaperAnalysis/ResearchPaperAnalysisView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IProps {
+
+}
+
+const ResearchPaperAnalysisView = (props: IProps) => {
+  return (
+    <>
+      upload and then view is there too
+    </>
+  )
+}
