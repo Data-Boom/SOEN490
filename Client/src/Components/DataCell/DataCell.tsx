ea1d83af47f82d20a6af792fb1a5c1879afbbf96
dsemenov <ds1618033@gmail.com>
2020-12-21 21:55:22 -0500

    made file upload into a generic component

---
+++ Client/src/Components/DataCell/DataCell.tsx
@@ -0,0 +1,51 @@
+import { FileUploadForm } from './FileUploadForm'
+import React from 'react'
+
+const fileFormat = 'application/json'
+export default function DataCell() {
+
+  const validateJson = (file: File) => {
+    return file && file.type !== fileFormat
+  }
+
+  /**
+     * Upon submission, the JSON file is extracted from the event and must be appended to formData
+     * to be sent with API request.
+     */
+  const handleSubmit = async (jsonFile: File) => {
+    const formData = new FormData()
+    formData.append('file', jsonFile)
+
+    const options = {
+      method: 'POST',
+      body: formData,
+    }
+
+    try {
+      await fetch('http://localhost:4000/dataupload', options)
+        .then(resp => resp.json())
+        .then(result => {
+          console.log(result[0])
+        })
+    }
+    catch (err) {
+      console.log('wrong file submitted, only json file accepted')
+    }
+  }
+
+  //Function to get the extension of a file
+  function getExtension(filename) {
+    let i = filename.lastIndexOf('.')
+    return (i < 0) ? '' : filename.substr(i)
+  }
+
+  return (
+    <>
+      <FileUploadForm
+        onSubmit={handleSubmit}
+        validateFile={validateJson}
+        acceptFileFormat={fileFormat}
+      />
+    </>
+  )
+}
---
+++ Client/src/Components/DataCell/FileUploadForm.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+import { Alert } from '@material-ui/lab'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import { Container } from '@material-ui/core'
+import { Snackbar } from '@material-ui/core'
+import { SyntheticEvent } from 'react'
+import { classStyles } from '../../appTheme'
+import uploadimage from './uploadimage.png'
+
+interface IProps {
+  acceptFileFormat: string,
+  onSubmit: (uploadedFile: File) => void,
+  //return true if file isValid
+  validateFile: (uploadedFile: File) => boolean,
+}
+
+const fileInputId = 'fileInput'
+
+export const FileUploadForm = (props: IProps) => {
+  const { onSubmit, validateFile, acceptFileFormat } = props
+
+  const [alertOpen, setAlertOpen] = useState(false)
+
+  const handleSubmit = (event: SyntheticEvent<HTMLFormElement, Event>) => {
+    const input: HTMLInputElement = event.target[fileInputId]
+    const file: File = input.files[0]
+    const isValidFile = validateFile(file)
+    if (isValidFile) {
+      onSubmit(file)
+    }
+    else {
+      setAlertOpen(true)
+    }
+  }
+
+  return (
+    <>
+      {/* Snackbar is used to show an error on the screen when a wrong file type is selected for uploading */}
+      <Snackbar open={alertOpen} autoHideDuration={3000} onClose={() => setAlertOpen(false)}>
+        <Alert onClose={() => setAlertOpen(false)} severity="error">
+          Failed to parse json
+        </Alert>
+      </Snackbar>
+
+      <Container>
+        <Box className={classStyles().defaultBorder}>
+          <img src={uploadimage} alt="Visual of clouds"></img>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input type="file" accept={acceptFileFormat} id={fileInputId} />
+              <Button variant="contained" type='submit' color='primary'> Upload this file! </Button>
+            </div>
+          </form>
+        </Box>
+      </Container>
+    </>
+  )
+}
