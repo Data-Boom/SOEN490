13c0577975c302452e0af7865d5ff88556bc577c
dsemenov <ds1618033@gmail.com>
2020-12-22 16:58:21 -0500

    got material ui to work clean with formik

--- Client/src/Components/DatasetUpload/DatasetUploadForm.tsx
+++ Client/src/Components/DatasetUpload/DatasetUploadForm.tsx
@@ -1,9 +1,12 @@
+import * as Yup from 'yup'
+
 import { Button, Typography } from '@material-ui/core'
 import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { IData, IDatasetMeta, IDatasetModel, IMaterial, IReference } from '../../Models/Datasets/IDatasetModel'
 
 import { MetaForm } from './MetaSection/MetaForm'
 import React from 'react'
+import { ReferenceForm } from './ReferenceSection/ReferenceForm'
 
 interface IProps {
   materials: IMaterial[],
@@ -32,50 +35,25 @@ export const DatasetUploadForm = (props: IProps): any => {
     onSubmit(dataset)
   }
 
-  interface Values {
-    firstName: string;
-    lastName: string;
-    email: string;
-  }
+  const validationSchema = Yup.object().shape({
+    firstname: Yup.string().required(),
+    middlename: Yup.string(),
+    lastname: Yup.string().required(),
+  })
 
   return (
     <div>
-      <h1>Signup</h1>
       <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-        }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>
-        ) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
-        }}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
       >
         <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="John" />
+          <MetaForm materials={materials} />
+          <ReferenceForm />
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
-
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="john@acme.com"
-            type="email"
-          />
-
-          <button type="submit">Submit</button>
         </Form>
       </Formik>
     </div>
   );
-};
 }
