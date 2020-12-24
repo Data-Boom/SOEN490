ca981186ec2c93bfa174cb89dc13c29a187f5b28
Arrow105 <sean.how0@gmail.com>
2020-12-24 03:59:54 -0500

    [#175] Added tests

---
+++ Server/src/tests/getBasicDataService.spec.ts
@@ -0,0 +1,34 @@
+import { createConnection, getConnection } from 'typeorm';
+import { retrieveBasicData } from '../services/getBasicDataService';
+
+describe('basic data service test', () => {
+    let retrieveBasicDataObject: retrieveBasicData;
+    jest.setTimeout(60000)
+
+    beforeAll(async () => {
+        await createConnection();
+        retrieveBasicDataObject = new retrieveBasicData();
+    });
+
+    afterAll(async () => {
+        await getConnection().close();
+    });
+
+    test('Asks for all categories, expects at least one category returned', async done => {
+        let arrayOfData = await retrieveBasicDataObject.getBasicCategoryData();
+        expect(arrayOfData[0].id).not.toBeUndefined()
+        done()
+    });
+
+    test('Asks for all subcategories, expects at least one subcategory returned', async done => {
+        let arrayOfData = await retrieveBasicDataObject.getBasicSubcategoryData();
+        expect(arrayOfData[0].id).not.toBeUndefined()
+        done()
+    });
+
+    test('Asks for all materials, expects at least one material returned', async done => {
+        let arrayOfData = await retrieveBasicDataObject.getBasicMaterialData();
+        expect(arrayOfData[0].id).not.toBeUndefined()
+        done()
+    });
+})
