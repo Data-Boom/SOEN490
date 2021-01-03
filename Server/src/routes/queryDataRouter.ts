25ce8693dd74b58857829ade96b0ff394f87e968
Mehrdad Ahmadi <mehrdad.ahmadi@bell.ca>
2021-01-03 13:18:56 -0500

    User's saved datasets endpoint

--- Server/src/routes/queryDataRouter.ts
+++ Server/src/routes/queryDataRouter.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import { getConnection } from 'typeorm';
 import { getDataController } from '../controllers/getDataController';
 
 /**
@@ -8,6 +9,27 @@ import { getDataController } from '../controllers/getDataController';
  */
 
 let router = Router();
+
+/**
+ * This route needs a user ID in its query param to be able to find the user in database
+ * Once user has been found, it returns a JSONified version of the saved datasets and the user's ID
+ */
+router.get('/datasets', (request: Request, response: Response) => {
+    let userID = request.query.userID;
+    let connection = getConnection();
+    try {
+        let userDataSets = await connection.manager.createQueryBuilder(User, 'user').select(DataSets, 'datasets').where('user.id = :id', { id: userID }).getMany();
+        if (userDataSets.length == 0) {
+            response.status(200).json({ id: userID, savedData: userDataSets });
+        }
+        else {
+            response.status(400).json("User does not have any saved datasets");
+        }
+    }
+    catch (error) {
+        response.status(400).json("User was not found");
+    }
+});
 
 router.get('/dataset*', (request: Request, response: Response) => {
     let getDataControllerObject = new getDataController();
