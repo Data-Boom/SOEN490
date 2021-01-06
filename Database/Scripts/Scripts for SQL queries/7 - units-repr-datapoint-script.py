0cd216d6e2ae06ca840bfdd46b4de0c17f419d44
Jeremiah <jeremiahteee@gmail.com>
2021-01-05 20:10:53 -0500

    [#148] add script to generate units, repr, datapoint queries

---
+++ Database/Scripts/Scripts for SQL queries/7 - units-repr-datapoint-script.py
@@ -0,0 +1,57 @@
+# This python script produces the units-repr-query.txt file containing the queries used to populate the 
+# units, representations, and datasetdatatype table.
+# The JSON used is the datasets-with-datapoints.json which was produced by the datasets-builder.py script found in Builders folder.
+
+import json
+
+file = open('./Queries in txt/7 - units-repr-datapoint-query.txt', "w+")
+datapointsFile = open('./Queries in txt/9 - datapoints-query.txt', "w+")
+
+with open('./Scraped JSONs/datasets-with-datapoints.json') as f:
+  data = json.load(f)
+  
+file.write("SET FOREIGN_KEY_CHECKS=0;\n")
+
+list_of_units = dict()
+list_of_representations = dict()
+
+unitCount = 2
+reprCount = 2
+
+for x in data: # Loop through each dataset
+    
+  for y in data[x]['data']['variables']: # Loop each variable array 
+    # Build 3 sets of txt files with SQL queries
+    
+    # Units query
+    if y['units'] not in list_of_units and y['units'] != "" and y['units'] != "N/A":
+      list_of_units[y['units']] = {"name": y['units'], "id": unitCount}
+      
+      file.write(" INSERT INTO `databoom_db_test`.`units` (`id`, `name`, `units`) VALUES ("+str(unitCount)+", \""+y['units']+"\", \""+y['units']+"\");\n")
+      
+      y['unitsId'] = list_of_units[y['units']]['id']
+      unitCount += 1
+    elif y['units'] == "" or y['units'] == "N/A":
+      y['unitsId'] = 1
+    else:
+      y['unitsId'] = list_of_units[y['units']]['id']
+    
+    # Representations query
+    
+    if y['repr'] not in list_of_representations and y['repr'] != "" and y['repr'] != "N/A":
+      list_of_representations[y['repr']] = {"name": y['repr'], "id": reprCount}
+      
+      file.write(" INSERT INTO `databoom_db_test`.`representations` (`id`, `repr`) VALUES ("+str(reprCount)+", \""+y['repr']+"\");\n")
+      
+      y['reprId'] = list_of_representations[y['repr']]['id']
+      reprCount += 1
+    elif y['repr'] == "" or y['repr'] == "N/A":
+      y['reprId'] = 1
+    else:
+      y['reprId'] = list_of_representations[y['repr']]['id']
+    
+    # Datapoints query
+    datapointsFile.write(" INSERT INTO `databoom_db_test`.`datapoints`(`datasetId`, `name`, `values`, `unitsId`, `representationsId`) VALUES ("+str(data[x]['datasetId'])+", \""+y['name']+"\", \""+str(y['value'])+"\", "+str(y['unitsId'])+", "+str(y['reprId'])+");\n")
+    
+file.close()
+datapointsFile.close()
