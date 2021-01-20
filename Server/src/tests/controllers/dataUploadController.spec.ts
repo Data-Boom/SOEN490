411db4a490df364da35231340dce88235aaf9160
Gurinder Dheer <gindulink@gmail.com>
2021-01-20 16:10:31 -0500

    [#161] Add integration tests for data upload controller

---
+++ Server/src/tests/controllers/dataUploadController.spec.ts
@@ -0,0 +1,51 @@
+import { DataUploadController } from '../../controllers/dataUploadController';
+import { Request, Response } from 'express';
+import { createConnection, getConnection } from 'typeorm';
+import { validTestData, inValidTestData } from '../testData/testData';
+
+describe('Data Upload Controller', () => {
+    let mockRequest
+    let mockResponse
+    let dataUploadController: DataUploadController
+
+    beforeEach(async () => {
+        await createConnection();
+        jest.setTimeout(60000)
+        dataUploadController = new DataUploadController()
+        mockRequest = {}
+        mockResponse = {
+            status: jest.fn(() => mockResponse),
+            json: jest.fn(),
+            send: jest.fn()
+        }
+
+    })
+    afterEach(async () => {
+        await getConnection().close();
+    });
+
+    test('Valid Json Upload', async () => {
+
+        let expectedResponse = "Upload to Database was successful!"
+        mockRequest = {
+            body: validTestData
+        }
+        await dataUploadController.createRequest(mockRequest as Request, mockResponse as Response)
+        expect(mockResponse.json).toBeCalledWith(expectedResponse)
+        expect(mockResponse.status).toBeCalledWith(200)
+    })
+
+    test('Invalid Json Upload', async () => {
+
+        let expectedResponse = "Type is a required field"
+        mockRequest = {
+            body: inValidTestData
+        }
+        await dataUploadController.createRequest(mockRequest as Request, mockResponse as Response)
+        expect(mockResponse.json).toBeCalledWith(expectedResponse)
+        expect(mockResponse.status).toBeCalledWith(400)
+    })
+
+
+
+})
