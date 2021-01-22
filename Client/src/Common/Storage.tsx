83a76924b4af2d162ce209fe47662ab4750adc5f
Vatika1 <35615793+Vatika1@users.noreply.github.com>
2021-01-21 19:56:54 -0500

    Update Storage.tsx by removing blanks

--- Client/src/Common/Storage.tsx
+++ Client/src/Common/Storage.tsx
@@ -3,7 +3,6 @@ import { IUserAccountModel } from "../Remote/Models/IUserAccountModel"
 //todo implement 'remember me' by putting the user into local storage
 export const putUserInStorage = (user: IUserAccountModel) => {
   sessionStorage.setItem('user', JSON.stringify(user))
-
 }
 
 export const getUserFromStorage = (): IUserAccountModel => {
@@ -11,6 +10,5 @@ export const getUserFromStorage = (): IUserAccountModel => {
 }
 
 export const removeUserInStorage = (user: IUserAccountModel) => {
-
   sessionStorage.removeItem('user');
 }
