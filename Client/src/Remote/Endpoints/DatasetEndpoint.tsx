3b5ad7857cf543aebebb4412a2a6a27bf31c00da
Rahimuz Zaman <rahimuz_z@yahoo.ca>
2021-01-22 15:21:33 -0500

    added endpoints for category, subcategory,material

--- Client/src/Remote/Endpoints/DatasetEndpoint.tsx
+++ Client/src/Remote/Endpoints/DatasetEndpoint.tsx
@@ -6,8 +6,38 @@ const userUploadedDatasetsRoute = '/dataset/userUploadedDatasets/:userUploadedDa
 const userSavedDatasetsRoute = '/dataset/userSavedDatsets/:userSavedDatsets'
 const datasetRoute = '/dataset*'
 
+interface ICategoryModel {
+  id: number,
+  name: string
+}
+interface ISubcategoryModel {
+  id: number,
+  name: string
+}
+interface IMaterialModel {
+  details: string
+  composition: string
+  id: number
+}
+
 export const getDatasets = async (query: ISearchDatasetsFormModel): Promise<any> => {
   const remoteDatasets = await get(datasetRoute, stringify(query))
   console.log(remoteDatasets)
   return remoteDatasets
+}
+
+export const listCategories = async (): Promise<ICategoryModel[]> => {
+  const categories = await get('/category')
+  console.log(categories)
+  return categories
+}
+export const listSubcategories = async (): Promise<ISubcategoryModel[]> => {
+  const subcategories = await get('/subcategory')
+  console.log(subcategories)
+  return subcategories
+}
+export const listMaterials = async (): Promise<IMaterialModel[]> => {
+  const materials = await get('/material')
+  console.log(materials)
+  return materials
 }
