7a77b7967d7cbe4f455be08849d80ba0c4674ed2
Rahimuz Zaman <rahimuz_z@yahoo.ca>
2021-01-25 20:01:51 -0500

    #263 fixed list spacing and code cleanup

--- Client/src/Components/Profile/ProfileGraphRow.tsx
+++ Client/src/Components/Profile/ProfileGraphRow.tsx
@@ -3,7 +3,8 @@ import { Link, Route } from 'react-router-dom'
 
 import { IGraphStateModel } from '../../Models/Graph/IGraphStateModel'
 import React from 'react'
-import { graphRoute } from '../../Common/Consts/Routes'
+import { classStyles } from '../../appTheme'
+
 
 interface IGraphModel {
     graphset: IGraphStateModel,
@@ -14,14 +15,16 @@ export const ProfileGraphRow = (props: IGraphModel) => {
     const { graphset } = { ...props }
 
     return (
-        <Grid>
-            <Box>
-                <Link to={'/graph/' + graphset.id} >
-                    <Typography>
-                        {graphset.name}
-                    </Typography>
-                </Link>
-            </Box>
+        <Grid container justify="center" spacing={3}>
+            <Grid item xs={3}>
+                <Box className={classStyles().datasetBorder}>
+                    <Link to={'/graph/' + graphset.id} >
+                        <Typography>
+                            {graphset.name}
+                        </Typography>
+                    </Link>
+                </Box>
+            </Grid>
         </Grid >
     )
 }
