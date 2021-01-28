f3df3c2cd6a81e07c1e6ab0f4b1655dd41418e0b
vatika <vatikaprasad@gmail.com>
2021-01-28 00:02:47 -0500

    fixed dataset enpoint file

--- Client/src/Remote/Endpoints/DatasetEndpoints.ts
+++ Client/src/Remote/Endpoints/DatasetEndpoints.ts
@@ -2,11 +2,10 @@ import { IDataSetModel } from './../../../../Server/src/genericInterfaces/DataPr
 import { IDatasetModel } from './../../Models/Datasets/IDatasetModel';
 import { get, post, put } from "../RemoteHelper"
 
-const dataset = '/api/v1/dataset'
+const datasetRoute = '/api/v1/dataset'
 const fetchDatasetRoute = '/api/v1/dataset/fetchUnapprovedDatasets$'
-//const rejectDatasetRoute = '/api/v1/dataset/:datasetId'
 const flagDatasetRoute = '/api/v1/flagDataSet/'
-const adminApprovedDatasetRoute = '/api/v1/adminApprovedDataset/:datasetId'
+const adminApprovedDatasetRoute = '/api/v1/adminApprovedDataset/'
 const userFlaggedDatasetsRoute = '/api/v1/userFlaggedDatasets'
 
 
@@ -20,10 +19,16 @@ export const flagDataset = async (datasetId: number, dataset: IDataSetModel): Pr
     return flaggedDataset
 }
 
-export const getUserFlaggedDatasets = async (): Promise<IDatasetModel> => {
-    const userFlaggedDatasets = await get(userFlaggedDatasetsRoute)
-    return userFlaggedDatasets
+export const callRejectDataset = async (datasetId: number): Promise<IDatasetModel> => {
+    const deletedDataset = await get(datasetRoute + '/' + datasetId)
+    return deletedDataset
 }
+
+export const adminApprovedDataset = async (datasetId: number, dataset: IDatasetModel): Promise<IDatasetModel> => {
+    const adminApprovedDataset = await put(adminApprovedDatasetRoute + '/' + datasetId, dataset)
+    return adminApprovedDataset
+}
+
 
 
 
