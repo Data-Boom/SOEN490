8932d82f9429aa9550943411ab2b29b2f067f4c5
Gurinder Dheer <gindulink@gmail.com>
2021-01-31 16:08:21 -0500

    try without seed file

--- Server/src/tests/controllers/authenticationController.spec.ts
+++ Server/src/tests/controllers/authenticationController.spec.ts
@@ -2,11 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import { createConnection, getConnection } from 'typeorm';
 
 import { AuthenticationController } from '../../controllers/authenticationController';
+import { Accounts } from '../../models/entities/Accounts';
+import { AuthenticationService } from '../../services/authenticationService';
 
 describe('Authentication Controller', () => {
     let mockRequest;
     let mockResponse;
     let next;
+    let connection
     let authenticationController: AuthenticationController;
 
     beforeEach(async () => {
@@ -21,9 +24,51 @@ describe('Authentication Controller', () => {
             setHeader: jest.fn()
         }
         next = {};
+
+        connection = getConnection();
+
+        // Accounts Data
+        let authenticationService = new AuthenticationService();
+
+        let user1 = new Accounts();
+        user1.id = 1;
+        user1.email = 'j.comkj';
+        user1.password = await authenticationService.hashPassword('123') as any;
+        user1.firstName = 'Ace';
+        user1.lastName = 'FireFist';
+        user1.dateOfBirth = '1980-01-01' as any;
+        user1.organizationName = 'Mugiwara';
+        user1.admin = !!true;
+        await connection.manager.save(user1);
+
+        let user2 = new Accounts();
+        user2.id = 2;
+        user2.email = 'test@t.com';
+        user2.password = await authenticationService.hashPassword('123') as any;
+        user2.firstName = 'Tom';
+        user2.lastName = 'Happy';
+        user2.dateOfBirth = '1980-01-01' as any;
+        user2.organizationName = 'Mobil';
+        user2.admin;
+        await connection.manager.save(user2);
+
+        let user3 = new Accounts();
+        user3.id = 3;
+        user3.email = 'tester@123.com';
+        user3.password = await authenticationService.hashPassword('123') as any;
+        user3.firstName = 'Wyatt';
+        user3.lastName = 'forfore';
+        user3.dateOfBirth = '1980-01-01' as any;
+        user3.organizationName = 'Ozark';
+        user3.admin = !!true;
+        await connection.manager.save(user3);
     });
 
     afterEach(async () => {
+
+        await connection.query('DELETE FROM accounts');
+        await connection.query('ALTER TABLE accounts AUTO_INCREMENT = 1');
+
         await getConnection().close();
     });
 
