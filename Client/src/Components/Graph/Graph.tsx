d149e1422c20ab7622864044b373b8584e8b2144
GD-Dheer <gindulink@gmail.com>
2021-02-15 17:59:31 -0500

    Revert "[#298] Frontned/graph optimization"

--- Client/src/Components/Graph/Graph.tsx
+++ Client/src/Components/Graph/Graph.tsx
@@ -1,7 +1,7 @@
 import * as am4charts from "@amcharts/amcharts4/charts"
 import * as am4core from "@amcharts/amcharts4/core"
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 
 import { Box } from "@material-ui/core"
 import { IAxisStateModel } from "../../Models/Graph/IGraphStateModel"
@@ -15,93 +15,50 @@ interface IProps {
 
 am4core.useTheme(am4themes_material)
 
-const getSeriesName = (dataset: IGraphDatasetModel) => {
-  return `${dataset.id}`
-}
-
 export default function Graph(props: IProps) {
   const { datasets, axes } = { ...props }
-  const chartRef = useRef<am4charts.XYChart>()
 
-  useEffect(() => initiateGraph(), [])
-  useEffect(() => chartRef.current && handleUnitsUpdated(), [axes[0].units, axes[1].units])
-  useEffect(() => chartRef.current && handleDatasetsUpdated(), [datasets])
+  useEffect(() => setUpGraph(), [datasets])
 
-  const handleUnitsUpdated = () => {
-    //cleanup series used for old variables
-    chartRef.current.series.clear()
-    const xAxis = chartRef.current.xAxes.getIndex(0)
-    const yAxis = chartRef.current.yAxes.getIndex(0)
-    updateAxis(xAxis, yAxis)
-  }
-
-  const handleDatasetsUpdated = () => {
-    pushAddedUpdatedDatasets(chartRef.current)
-    pullRemovedDatasets(chartRef.current)
-  }
-
-  const pushAddedUpdatedDatasets = (chart: am4charts.XYChart) => {
+  const addSeries = (chart) => {
     datasets.forEach(dataset => {
-      let datasetSeries = chart.series.values.find(series => series.name == getSeriesName(dataset))
+      const lineSeries = chart.series.push(new am4charts.CandlestickSeries())
+      const bullet = lineSeries.bullets.push(new am4charts.CircleBullet())
+      //todo make tooltip nice
+      bullet.tooltipText = `${dataset.name}
+      ${axes[0].variableName}: {${dataset.id}x} ${axes[0].units}
+      ${axes[1].variableName}: {${dataset.id}y} ${axes[1].units}`
 
-      if (!datasetSeries) {
-        datasetSeries = setUpSeries(chartRef.current, dataset)
-      }
-
-      //optimization to only set data if it wasnt set before
-      if (!datasetSeries.data || datasetSeries.data.length == 0) {
-        datasetSeries.data = dataset.points
-      }
-
-      toggleVisibilityIfNeed(datasetSeries, dataset)
+      lineSeries.dataFields.valueX = `${dataset.id}x`
+      lineSeries.dataFields.valueY = `${dataset.id}y`
     })
   }
 
-  const toggleVisibilityIfNeed = (datasetSeries: am4charts.Series, dataset: IGraphDatasetModel) => {
-    if (shouldHideDataset(datasetSeries, dataset)) {
-      datasetSeries.hide()
-      return
+  const buildDataForGraph = (datasets: IGraphDatasetModel[]): any[] => {
+    const data = []
+    for (let datasetIndex = 0; datasetIndex < datasets.length; datasetIndex++) {
+      const dataset = datasets[datasetIndex]
+      mergeDatasetPoints(dataset, data)
     }
 
-    if (shouldShowDataset(datasetSeries, dataset)) {
-      datasetSeries.show()
+    return data
+  }
+
+  const mergeDatasetPoints = (dataset: IGraphDatasetModel, data: any[]) => {
+    if (!dataset || dataset.isHidden) {
       return
     }
-  }
-
-  const shouldHideDataset = (datasetSeries: am4charts.Series, dataset: IGraphDatasetModel) => {
-    return dataset.isHidden && (!datasetSeries.isHidden || !datasetSeries.isHiding)
-  }
-
-  const shouldShowDataset = (datasetSeries: am4charts.Series, dataset: IGraphDatasetModel) => {
-    return !dataset.isHidden && (datasetSeries.isHidden || datasetSeries.isHiding)
-  }
-
-  const pullRemovedDatasets = (chart: am4charts.XYChart) => {
-    chart.series.values.forEach(series => {
-      if (!datasets.find(dataset => getSeriesName(dataset) == series.name)) {
-        chart.series.removeValue(series)
-      }
-    })
-  }
-
-  const setUpSeries = (chart: am4charts.XYChart, dataset: IGraphDatasetModel): am4charts.XYSeries => {
-    const datasetSeries = chart.series.push(new am4charts.XYSeries())
-    const bullet = datasetSeries.bullets.push(new am4core.Circle())
-    bullet.radius = 5
-    bullet.tooltipText = `${dataset.name}
-        ${axes[0].variableName}: {x} ${axes[0].units}
-        ${axes[1].variableName}: {y} ${axes[1].units}`
-
-    datasetSeries.dataFields.valueX = `x`
-    datasetSeries.dataFields.valueY = `y`
-    datasetSeries.name = getSeriesName(dataset)
-    datasetSeries.data = []
-    return datasetSeries
+    //for all current dataset points:
+    for (let pointIndex = 0; pointIndex < dataset.points.length; pointIndex++) {
+      //create new point that has previous points info and this one's
+      const mergedPoint = { ...data[pointIndex], [`${dataset.id}x`]: dataset.points[pointIndex].x, [`${dataset.id}y`]: dataset.points[pointIndex].y }
+      data[pointIndex] = mergedPoint
+    }
   }
 
   const subscribeToChartEvents = (chart: am4charts.XYChart, xAxis: am4charts.Axis, yAxis: am4charts.Axis) => {
     chart.events.on("ready", function () {
+      //todo zoom to values or indeces?
       axes[0].zoomStartIndex &&
         axes[0].zoomEndIndex &&
         xAxis.zoomToIndexes(axes[0].zoomStartIndex, axes[0].zoomEndIndex)
@@ -112,30 +69,30 @@ export default function Graph(props: IProps) {
     })
   }
 
-  const updateAxis = (xAxis: any, yAxis: any) => {
+  const updateAxis = (xAxis, yAxis) => {
     xAxis.title.text = `${axes[0].variableName}, ${axes[0].units}`
     xAxis.logarithmic = axes[0].logarithmic || false
     xAxis.renderer.minGridDistance = 40
-    xAxis.keepSelection = true
 
-    yAxis.title.text = `${axes[1].variableName}, ${axes[1].units}`
     yAxis.logarithmic = axes[1].logarithmic || false
-    yAxis.keepSelection = true
+    yAxis.title.text = `${axes[1].variableName}, ${axes[1].units}`
   }
 
-  const initiateGraph = () => {
-    chartRef.current = am4core.create("chartdiv", am4charts.XYChart)
+  const setUpGraph = () => {
+    const chart = am4core.create("chartdiv", am4charts.XYChart)
+    chart.data = buildDataForGraph(datasets)
 
-    chartRef.current.scrollbarX = new am4core.Scrollbar()
-    chartRef.current.scrollbarY = new am4core.Scrollbar()
-    chartRef.current.cursor = new am4charts.XYCursor()
-    chartRef.current.cursor.behavior = "zoomXY"
-    chartRef.current.exporting.menu = new am4core.ExportMenu()
+    const xAxis = chart.xAxes.push(new am4charts.ValueAxis())
+    const yAxis = chart.yAxes.push(new am4charts.ValueAxis())
+    updateAxis(xAxis, yAxis)
+    addSeries(chart)
 
-    const xAxis = chartRef.current.xAxes.push(new am4charts.ValueAxis())
-    const yAxis = chartRef.current.yAxes.push(new am4charts.ValueAxis())
-    subscribeToChartEvents(chartRef.current, xAxis, yAxis)
-    chartRef.current = chartRef.current
+    chart.scrollbarX = new am4core.Scrollbar()
+    chart.scrollbarY = new am4core.Scrollbar()
+    chart.cursor = new am4charts.XYCursor()
+    chart.exporting.menu = new am4core.ExportMenu()
+
+    subscribeToChartEvents(chart, xAxis, yAxis)
   }
 
   return (
