d6e3727447836070b95526762aeb3768467afd2c
Gurinder Dheer <gindulink@gmail.com>
2021-02-15 18:46:20 -0500

    [#395] Add modal footer

--- Client/src/Components/UserReview/ModalFooter.tsx
+++ Client/src/Components/UserReview/ModalFooter.tsx
@@ -1,24 +1,21 @@
-import { Box, Grid, Link, Typography } from '@material-ui/core';
-
-import { IApprovalDatasetModel } from '../../../../Server/src/models/interfaces/DatasetModelInterface';
+import { Button } from '@material-ui/core';
 import { IApprovedDatasetModel } from '../../Models/Datasets/IApprovedDatasetModel';
-import React, { useState } from 'react';
-import { classStyles } from '../../appTheme';
-import { callRejectDataset } from '../../Remote/Endpoints/DatasetEndpoint';
-
+import React from 'react';
+import { IDatasetModel } from '../../Models/Datasets/IDatasetModel';
 
 interface IProps {
-    onApprovalChange(): void
-    onRejectChange(): void
-    onSubmit(): void
+    dataset: IDatasetModel,
+    handleApproveDataset: (dataset: IApprovedDatasetModel) => void
+    handleDeleteDataset: () => void
+    handleEditDataset: () => void
 }
 
-export const ModalFooter = () => {
-
-    const [editable, setEditable] = useState(false)
-
-    const handleEditDataset = () => {
-        setEditable(!editable)
-    }
-
+export const ModalFooter = (props: IProps) => {
+    return (
+        <>
+            <Button size="small" id="edit-dataset" onClick={() => props.handleEditDataset} color="primary" variant="contained">{'Edit Dataset'}</Button>
+            <Button size="small" id="approve-dataset" onClick={() => props.handleApproveDataset} color="primary" variant="contained">{"Approve Dataset"}</Button>
+            <Button size="small" id="delete-dataset" onClick={() => props.handleDeleteDataset} color="primary" variant="contained">{'Delete Dataset'}</Button>
+        </>
+    )
 }
