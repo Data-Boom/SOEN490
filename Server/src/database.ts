bbe1d16d507fd209a0a633b8544494611c8ac5d7
Mehrdad Ahmadi <mehrdad2maskull@gmail.com>
2021-02-16 21:22:38 -0500

    #286-dimensions-api: Auto stash before merge of "#286-dimensions-api" and "origin/#286-dimensions-api"

--- Server/src/database.ts
+++ Server/src/database.ts
@@ -4,6 +4,28 @@ export const connectDB = async (config: any) => {
   await createConnection(config).then(async connection => {
     console.log("Connection was made.")
 
+    let unit = new Units();
+    unit.conversionFormula = "conversion";
+    unit.name = "some name";
+
+    await Units.save([unit]);
+
+    let secondUnit = new Units();
+    secondUnit.conversionFormula = "Some conversion formula";
+    secondUnit.name = "Second unit";
+
+    await Units.save([secondUnit]);
+
+    console.log("Must have added the unit");
+
+    let dimension = new Dimension();
+    dimension.name = "Temperatuer";
+    dimension.units = [unit, secondUnit];
+
+    await Dimension.save([dimension]);
+
+    console.log("Must have added the dimension");
+
     //Methods for Entity Manager: https://github.com/typeorm/typeorm/blob/master/docs/entity-manager-api.md
     // This is using connection.manager.{methodName}
 
