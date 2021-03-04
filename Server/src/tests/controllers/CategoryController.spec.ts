5da75d37922f1c9427ccc4a4c63bf208f3365457
Arrow105 <sean.how0@gmail.com>
2021-03-04 15:51:37 -0500

    [#358] Added some tests

---
+++ Server/src/tests/controllers/CategoryController.spec.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import { createConnection, getConnection } from 'typeorm';
+import { CategoryController } from '../../controllers/CategoryController';
+
+describe('Category Controller', () => {
+    let mockRequest;
+    let mockResponse;
+    let controller: CategoryController;
+    let validUploadData = {
+        "name": "new category",
+        "subcategories": [
+            {
+                "name": "created subcategory"
+            }
+        ]
+    }
+
+    beforeEach(async () => {
+        await createConnection();
+        jest.setTimeout(60000)
+        controller = new CategoryController();
+        mockRequest = {};
+        mockResponse = {
+            status: jest.fn(() => mockResponse),
+            json: jest.fn(),
+        }
+    });
+
+    afterEach(async () => {
+        await getConnection().close();
+    });
+
+    test('Valid Get All Categories Request; expect at least one entry in return', async () => {
+        await controller.retrieveCategories(mockResponse as Response)
+        expect(mockResponse.json[0]).not.toBeUndefined;
+        expect(mockResponse.status).toBeCalledWith(200);
+    });
+
+    test('Valid Create Category Request; expect at least one entry in return', async () => {
+        mockRequest = {
+            body: validUploadData
+        }
+        await controller.createCategory(mockRequest as Request, mockResponse as Response)
+        expect(mockResponse.json).not.toBeUndefined;
+        expect(mockResponse.status).toBeCalledWith(201);
+    });
+
+    test('Invalid Create Category Request', async () => {
+        mockRequest = {
+            body: {}
+        }
+        await controller.createCategory(mockRequest as Request, mockResponse as Response)
+        expect(mockResponse.json).toBeCalledWith("Request is invalid. Please enter a category name");
+        expect(mockResponse.status).toBeCalledWith(400);
+    });
+})
