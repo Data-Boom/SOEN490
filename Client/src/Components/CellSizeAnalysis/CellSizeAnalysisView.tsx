817fe420b951d8844a1515a65f1b05f1e8f10f76
Jeremiah <jeremiahteee@gmail.com>
2021-03-07 22:54:57 -0500

    test out different download links for exe (not working)

--- Client/src/Components/CellSizeAnalysis/CellSizeAnalysisView.tsx
+++ Client/src/Components/CellSizeAnalysis/CellSizeAnalysisView.tsx
@@ -1,79 +1,26 @@
-import { Box, Typography, useTheme } from '@material-ui/core'
-import React, { useState } from 'react'
+import { Box, Typography } from '@material-ui/core'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Download from '@axetroy/react-download'
 
-import { DatasetView } from '../DatasetUpload/DatasetView'
-import { FileUploadForm } from '../FileUpload/FileUploadForm'
-import { IDatasetModel } from '../../Models/Datasets/IDatasetModel'
-import Loader from "react-loader-spinner"
-import { exampleExportDatasetModel } from '../../Models/Datasets/IDatasetModel'
 
-interface IProps {
-
-}
-
-const fileFormat = 'image/ppm'
-
-export const CellSizeAnalysisView = (props: IProps) => {
-
-  const [analyzedDataset, setAnalyzedDataset] = useState<IDatasetModel>(null)
-  const [isProcessingCellSize, setIsProcessingCellSize] = useState(false)
-
-  const handleSubmit = async (cell: File): Promise<void> => {
-    console.log("handle submit")
-    analyzeCellSize(cell)
-  }
-
-  const analyzeCellSize = async (cell: File): Promise<void> => {
-    console.log("analyze cell size")
-    setIsProcessingCellSize(true)
-    const fetchedDataset = await fetchDataset()
-    setIsProcessingCellSize(false)
-    setAnalyzedDataset(fetchedDataset)
-  }
-
-  const fetchDataset = (): Promise<IDatasetModel> => {
-    console.log("Fetch dataset")
-    //todo make an actual API call providing a cell
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(exampleExportDatasetModel)
-      }, 2000)
-    })
-  }
-  const validatePPM = (file: File) => {
-    console.log(file.type)
-    return file && file.type === fileFormat
-  }
-  const theme = useTheme()
-
-  const cellUploadSection = (): any => {
-    return (
-      <>
-        <Box pt={5}>
-          <Typography>Cell Size Analysis</Typography>
-          <FileUploadForm
-            onSubmit={handleSubmit}
-            isValidFile={validatePPM}
-            acceptFileFormat={fileFormat}
-          />
-        </Box>
-      </>
-    )
-  }
-
+export const CellSizeAnalysisView = () => {
   return (
     <>
-      {!analyzedDataset ?
-        cellUploadSection() :
-        <DatasetView initialDataset={analyzedDataset} />
-      }
-      <Loader
-        type='Bars'
-        color={theme.palette.secondary.main}
-        height={100}
-        width={100}
-        visible={isProcessingCellSize}
-      />
+      <Box p={5}>
+        <h1> Cell Size Analysis Tool </h1>
+        <h2> Instructions </h2>
+        <Typography>To start an analysis, enter the full path of the image and the top to bottom length of the image. Adjust the sigma, low and high threhsold sliders to get the preferred image.</Typography>
+        <Typography>Small sigma values allow detect of small sharp lines and fine features. High sigma detects large edges.</Typography>
+        <Typography> Low and high thresholds determine which gradient edge pixels are kept. </Typography>
+        <h2>Installation</h2>
+        <Typography>Downloada the installer below supported on: Windows, Mac OS, Linux. </Typography>
+        <a href="./python-3.9.2-amd64.exe" download>Download CellSizeAnalyzer.exe</a><br></br>
+        <Link to="./python-3.9.2-amd64.exe" target="_blank" download>Download CellSizeAnalyzer.exe</Link>
+        <Download file="CellSizeAnalyzer.exe" content={require("./python-3.9.2-amd64.exe")}>
+          <button type="button">Download CellSizeAnalyzer.exe</button>
+        </Download>
+      </Box>
     </>
   )
 }
