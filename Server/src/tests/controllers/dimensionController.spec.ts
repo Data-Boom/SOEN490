abc5c9245746f3d05b38b07bc98b6ce565ccae36
Mehrdad Ahmadi <mehrdad2maskull@gmail.com>
2021-03-14 16:31:03 -0400

    Unit Tests for Dimension COntroller

--- Server/src/tests/controllers/dimensionController.spec.ts
+++ Server/src/tests/controllers/dimensionController.spec.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
 import { createConnection, getConnection } from 'typeorm';
-
+import { validDimensionData, invalidDeleteDimensionDataUnitInUse, invalidDimensionData, availableDBDimensions, duplicateDimensionData, validDeleteDimensionData, validUpdateDimensionData } from '../testData/dimensionTestData';
 import { DimensionsController } from '../../controllers/DimensionsController';
 
 describe('Dimensions Controller', () => {
+  let dimensionsController: DimensionsController;
   let mockRequest;
   let mockResponse;
-  let next;
-  let dimensionsController: DimensionsController;
 
   beforeEach(async () => {
     await createConnection();
@@ -16,58 +15,38 @@ describe('Dimensions Controller', () => {
     mockRequest = {};
     mockResponse = {
       status: jest.fn(() => mockResponse),
-      cookie: jest.fn(),
       json: jest.fn(),
-      setHeader: jest.fn()
     }
-    next = {};
   });
 
   afterEach(async () => {
     await getConnection().close();
   });
 
-  // Test Cases for Create Dimension
+  // Test Cases for Get Dimension
 
-  test('Valid Create Dimension Request', async () => {
-    const expectedResponse = {
-      "baseUnitId": 7761,
-      "id": 13,
-      "name": "Velocity",
-      "units": [
-        {
-          "conversionFormula": "{u}",
-          "dimensionId": 13,
-          "id": 7761,
-          "name": "m/s"
-        }
-      ]
-    };
-    mockRequest = {
-      body: {
-        name: "Velocity",
-        units: [
-          {
-            name: "m/s",
-            conversionFormula: "{u}"
-          }
-        ]
-      }
-    }
-    await dimensionsController.createDimension(mockRequest as Request, mockResponse as Response)
+  test('Valid Request to Get Dimensions', async () => {
+    const expectedResponse = availableDBDimensions;
+    await dimensionsController.retrieveDimensions(mockResponse as Response);
+    expect(mockResponse.status).toBeCalledWith(200);
     expect(mockResponse.json).toBeCalledWith(expectedResponse);
-    expect(mockResponse.status).toBeCalledWith(201);
   });
 
+  // Test Cases for Create Dimension
+  test('Valid Create Dimension Request', async () => {
+    mockRequest = {
+      body: validDimensionData
+    }
+    await dimensionsController.createDimension(mockRequest as Request, mockResponse as Response)
+    expect(mockResponse.json).not.toBeUndefined;
+    expect(mockResponse.status).toBeCalledWith(201);
+  });
 
   test('Invalid Create Dimension Request due to missing parameters - Error 400', async () => {
     const expectedResponse = "Request is invalid. Missing attributes";
     mockRequest = {
-      body: {
-        unit: 0
-      }
+      body: invalidDimensionData
     }
-
     await dimensionsController.createDimension(mockRequest as Request, mockResponse as Response)
     expect(mockResponse.status).toBeCalledWith(400);
     expect(mockResponse.json).toBeCalledWith(expectedResponse);
@@ -76,15 +55,7 @@ describe('Dimensions Controller', () => {
   test('Invalid Create Dimension with duplicate entry - Error 400', async () => {
     const expectedResponse = "This dimension already exists! Please use different values";
     mockRequest = {
-      body: {
-        name: "Velocity",
-        units: [
-          {
-            name: "m/s",
-            conversionFormula: "{u}"
-          }
-        ]
-      }
+      body: duplicateDimensionData
     }
 
     await dimensionsController.createDimension(mockRequest as Request, mockResponse as Response)
@@ -92,52 +63,12 @@ describe('Dimensions Controller', () => {
     expect(mockResponse.json).toBeCalledWith(expectedResponse);
   });
 
-  // Test Cases for Get Dimension
-
-  test('Valid Request to Get Dimensions', async () => {
-    const expectedResponse = [
-      {
-        "baseUnitId": null,
-        "id": 2,
-        "name": "Density",
-        "units": [
-
-        ]
-      },
-      {
-        "baseUnitId": 7761,
-        "id": 13,
-        "name": "Velocity",
-        "units": [
-          {
-            "conversionFormula": "{u}",
-            "dimensionId": 13,
-            "id": 7761,
-            "name": "m/s"
-          }
-        ]
-      }
-    ];
-    await dimensionsController.retrieveDimensions(mockResponse as Response);
-    expect(mockResponse.status).toBeCalledWith(200);
-    expect(mockResponse.json).toBeCalledWith(expectedResponse);
-  });
-
   // Test Cases for Updating Dimension
 
   test('Valid Update Dimension Request', async () => {
-    const expectedResponse = "Success";
+    const expectedResponse = validUpdateDimensionData
     mockRequest = {
-      body: {
-        name: "Velocity",
-        id: 1,
-        units: [
-          {
-            name: "m/s",
-            conversionFormula: "{u^2}"
-          }
-        ]
-      }
+      body: validUpdateDimensionData
     }
     await dimensionsController.updateDimension(mockRequest as Request, mockResponse as Response)
     expect(mockResponse.json).toBeCalledWith(expectedResponse);
@@ -147,9 +78,7 @@ describe('Dimensions Controller', () => {
   test('Invalid Update Dimension Request due to missing parameters - Error 400', async () => {
     const expectedResponse = "Request is invalid. Missing attributes";
     mockRequest = {
-      body: {
-        unit: 0
-      }
+      body: invalidDimensionData
     }
 
     await dimensionsController.createDimension(mockRequest as Request, mockResponse as Response)
@@ -162,14 +91,21 @@ describe('Dimensions Controller', () => {
   test('Valid Case to Delete Dimension', async () => {
     const expectedResponse = "Success";
     mockRequest = {
-      params: {
-        id: 1
-      }
+      params: validDeleteDimensionData
     }
-
     await dimensionsController.deleteDimension(mockRequest as Request, mockResponse as Response)
-    expect(mockResponse.status).toBeCalledWith(200);
     expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    expect(mockResponse.status).toBeCalledWith(200);
+  });
+
+  test('Invalid Case to Delete Dimension - Unit in use', async () => {
+    const expectedResponse = "Can't remove a unit as it is used by one or more data points";
+    mockRequest = {
+      params: invalidDeleteDimensionDataUnitInUse
+    }
+    await dimensionsController.deleteDimension(mockRequest as Request, mockResponse as Response)
+    expect(mockResponse.status).toBeCalledWith(400);
+    expect(mockResponse.json).toBeCalledWith(expectedResponse)
   });
 
 })
