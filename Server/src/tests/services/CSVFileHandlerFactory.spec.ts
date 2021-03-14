63fe150ff0f6d92411d7d6660fb8ecb276d76dd1
Mehrdad Ahmadi <mehrdad2maskull@gmail.com>
2021-03-13 22:13:23 -0400

    Fix unit tests

--- Server/src/tests/services/CSVFileHandlerFactory.spec.ts
+++ Server/src/tests/services/CSVFileHandlerFactory.spec.ts
@@ -1,25 +1,24 @@
 import { CSVFileExtractorFactory } from "../../services/dataExtraction/CSVFileHandlerFactory";
 
 describe('CSF Parser tests', () => {
-    const fileSystem = require('fs');
+
+    let expectation = [{ "Initial Pressure": "101", "Initial Temperature": "293", "Cell Size": "12" }, { "Initial Pressure": "50", "Initial Temperature": "293", "Cell Size": "24" }, { "Initial Pressure": "25", "Initial Temperature": "293", "Cell Size": "48" }]
 
     let csvFile = "src/tests/testData/csv_data_upload_sample.csv"
     let txtFile = "src/tests/testData/txt_csv_data_upload_sample.txt"
 
     let csvFileFactory = new CSVFileExtractorFactory()
 
-    let expectation = [{ "Initial Pressure": "101", "Initial Temperature": "293", "Cell Size": "12" }, { "Initial Pressure": "50", "Initial Temperature": "293", "Cell Size": "24" }, { "Initial Pressure": "25", "Initial Temperature": "293", "Cell Size": "48" }]
-
     test('Test parsing CSV file', async () => {
         let fileHandler = csvFileFactory.getFileHandler(csvFile)
-        let parsedData = fileHandler.parseFile()
-        expect(parsedData).toBe(expectation);
+        let parsedData = await fileHandler.parseFile()
+        expect(parsedData).toEqual(expectation);
     });
 
     test('Test parsing txt file', async () => {
         let fileHandler = csvFileFactory.getFileHandler(txtFile)
-        let parsedData = fileHandler.parseFile()
-        expect(parsedData).toBe(expectation);
+        let parsedData = await fileHandler.parseFile()
+        expect(parsedData).toEqual(expectation);
     });
 
 })
