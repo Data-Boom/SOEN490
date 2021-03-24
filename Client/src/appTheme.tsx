f3adcc4c02697a2c38e31996f044465f3ffee4e6
vatika <vatikaprasad@gmail.com>
2021-03-24 16:36:41 -0400

    kiyanda approved colour theme

--- Client/src/appTheme.tsx
+++ Client/src/appTheme.tsx
@@ -5,30 +5,7 @@ export const theme = createMuiTheme({
   palette: {
     primary: {
       light: '#757ce8',
-      //main: '#455A64',
-      // main: '#2196F3',
-      //main: '#587389',
-      //main: '#779CBB',
-      //main: '#61686E',
-      //main: '#675A7E',
-      //main: '#421410',
-      //main: '#7C524E',
-      //main: '#677967',
-      //main: '#3f50b5', //CURRENT ONE
-      main: '#7F95BB', //Kiyanda's choice
-      //main: '#1C7E4D', //greenish
-      //main: '#673AB7', //purple
-      //main: '#FF9800', //orangish
-      //main: '#EC7D5A',//coral
-      //main: '#F44336',//reddish
-      //main: '#AB149E', //pinkish purple
-      //main: '#4FC3F7', //sky blue
-      //main: '#1273DE', //blue
-      //main: '#80AA50',
-      //main: '#AA507A', //dark pinkish
-      //main: '#7B64FF',//lavender
-      //main: '#9C84B1',
-      //main: '#7F95BB',
+      main: '#7F95BB',
       dark: '#002884',
       contrastText: '#fff',
     },
