4ea98eaf04b3a8365b2683d0a1f74da6c383b472
Laith Awad <layth_awad@hotmail.com>
2021-03-25 17:10:27 -0400

    created the dialog box for adding new material

--- Client/src/Components/DatasetUpload/DatasetForm/MetaSection/MaterialSelectChipArray.tsx
+++ Client/src/Components/DatasetUpload/DatasetForm/MetaSection/MaterialSelectChipArray.tsx
@@ -1,11 +1,13 @@
 //todo move views into their corresponding feature folders
 
-import { Grid, TextField, Typography } from '@material-ui/core'
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete'
+import { Button, DialogActions, DialogContent, DialogTitle, Grid, TextField, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
 
 import { ArrayHelpers } from 'formik'
-import Autocomplete from '@material-ui/lab/Autocomplete'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContentText from '@material-ui/core/DialogContentText/DialogContentText'
 import { IMaterial } from '../../../../Models/Datasets/IDatasetModel'
-import React from 'react'
 
 interface IProps {
   value: IMaterial[],
@@ -20,6 +22,11 @@ const materialToString = (material: IMaterial) => {
 
 export const MaterialSelectChipArray = (props: IProps) => {
   const { value, options, fieldArrayHelpers, editable } = props
+
+  const filter = createFilterOptions<IMaterial>()
+  const [open, toggleOpen] = useState(false)
+  const [dialogValue, setDialogValue] = useState<IMaterial>()
+
   const handleDelete = (materialToDelete: IMaterial) => {
     if (editable) {
       const indexToRemove = value.findIndex(material => materialToString(material) == materialToString(materialToDelete))
@@ -32,11 +39,59 @@ export const MaterialSelectChipArray = (props: IProps) => {
       if (!materialToString(newMaterial)) {
         return
       }
-
+      if (!options.includes(newMaterials[0])) {
+        setTimeout(() => {
+          toggleOpen(true)
+        })
+      }
       if (value.findIndex((material) => materialToString(material) == materialToString(newMaterial)) == -1) {
         fieldArrayHelpers.push(newMaterial)
       }
     })
+  }
+
+  const handleSubmitMaterial = () => {
+    toggleOpen(false)
+  }
+
+  const handleCloseMaterialForm = () => {
+    toggleOpen(false)
+  }
+  const handleAddNewMaterial = (newMaterials: IMaterial) => {
+
+
+    // return (
+    //   <Dialog open={open} onClose={handleCloseMaterialForm}>
+    //     <form onSubmit={handleSubmitMaterial}>
+    //       <DialogTitle> Add a new material</DialogTitle>
+    //       <DialogContent>
+    //         <TextField
+    //           autoFocus
+    //           margin="dense"
+    //           value={newMaterials.composition}
+    //           label="Composition"
+    //           type="text"
+    //         // onChange={(event)=> setDialogValue({ ...dialogValue, event.target.value})}
+    //         />
+    //         <TextField
+    //           autoFocus
+    //           margin="dense"
+    //           value={newMaterials.details}
+    //           label="details"
+    //           type="text"
+    //         />
+    //       </DialogContent>
+    //       <DialogActions>
+    //         <Button onClick={handleCloseMaterialForm} color="primary">
+    //           Cancel
+    //         </Button>
+    //         <Button type="submit" color="primary">
+    //           Add
+    //         </Button>
+    //       </DialogActions>
+    //     </form>
+    //   </Dialog>
+    // )
   }
 
   return (
@@ -50,8 +105,52 @@ export const MaterialSelectChipArray = (props: IProps) => {
             options={options}
             multiple
             getOptionLabel={option => materialToString(option)}
+            filterOptions={(options, params) => {
+              const filtered = filter(options, params)
+              if (params.inputValue !== '') {
+                filtered.push({
+                  composition: params.inputValue,
+                  details: params.inputValue,
+                  id: 1
+                })
+              }
+              return filtered
+            }}
             renderInput={(params) => <TextField {...params} label="Material" variant="outlined" />}
           />
+          <Dialog open={open} onClose={handleCloseMaterialForm}>
+            <form onSubmit={handleSubmitMaterial}>
+              <DialogTitle> Add a new material</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Please provide the composition and the details of the metal you want to add
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  //value={dialogValue.composition}
+                  label="Composition"
+                  type="text"
+                //onChange={(event)=> handleAddNewMaterial({ ...dialogValue, event.target.event})}
+                />
+                <TextField
+                  margin="dense"
+                  //value={dialogValue.details}
+                  //onChange={(event)=> handleAddNewMaterial({ ...dialogValue, event.target.event})}
+                  label="details"
+                  type="text"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleCloseMaterialForm} color="primary">
+                  Cancel
+            </Button>
+                <Button onClick={handleSubmitMaterial} color="primary">
+                  Add Material
+            </Button>
+              </DialogActions>
+            </form>
+          </Dialog>
         </Grid>
       </Grid>
     </>
