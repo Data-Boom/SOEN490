f63e85f9a86b0733aa104a75bc7fee57847432cf
Gurinder Dheer <gdheer@tactiohealth.com>
2021-03-30 16:24:11 -0400

    [#314] Update validation schemas

--- Client/src/Components/Profile/CategoryManagementSection/CategoryValidationSchema.tsx
+++ Client/src/Components/Profile/CategoryManagementSection/CategoryValidationSchema.tsx
@@ -1,6 +1,12 @@
 import * as Yup from 'yup'
 
 export const CategoryValidationSchema = Yup.object().shape({
-    categoryName: Yup.string().trim().required('Category Name is a required field'),
-    subCategoryName: Yup.string().trim().required('Subcategory Name is a required field'),
+    name: Yup.string().trim().required('Category Name is a required field'),
+    subcategories: Yup.array().of(
+        Yup.object().shape(
+          {
+            name: Yup.string().trim().required('Subcategory Name is a required field'),
+          }
+        )
+      ),
 })
