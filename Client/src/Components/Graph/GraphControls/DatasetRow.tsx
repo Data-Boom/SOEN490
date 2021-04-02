4207111fe4d58a0de594755dd773eac83e241b0e
Leslie <leslie.poso@hotmail.com>
2021-04-02 09:16:51 -0400

    Changed color picker to show color chosen. Moved buttons around.

--- Client/src/Components/Graph/GraphControls/DatasetRow.tsx
+++ Client/src/Components/Graph/GraphControls/DatasetRow.tsx
@@ -1,4 +1,4 @@
-import { Box, FormControl, Grid, IconButton, InputLabel, MenuItem, Select, Tooltip, Typography } from "@material-ui/core"
+import { Box, FormControl, Grid, IconButton, InputLabel, MenuItem, Select, Tooltip, Typography, makeStyles } from "@material-ui/core"
 import { IDatasetRowModel, shapes } from "../../../Models/Datasets/IDatasetRowModel"
 import React, { useState } from 'react'
 
@@ -16,11 +16,20 @@ export interface IDatasetRowProps {
   onChangeDatasetColor: (datasetId: number, color: string) => void
 }
 
+
 export const DatasetRow = (props: IDatasetRowProps | IRowProps<IDatasetRowModel>) => {
   const { row: datasetRow, onRemoveDatasetClick, onHideDatasetSwitch, onChangeDatasetShape, onChangeDatasetColor } = { ...props }
   const [isHidden, setIsHidden] = useState(datasetRow.isInitiallyHidden)
   const [color, setColor] = useState(datasetRow.color || '#000')
   const [shape, setShape] = useState(datasetRow.shape)
+
+  const colorStyle = () => {
+    return makeStyles({
+      colorPicker: {
+        background: color
+      },
+    })()
+  }
 
   const handleHiddenSwitch = () => {
     setIsHidden(!isHidden)
@@ -48,6 +57,34 @@ export const DatasetRow = (props: IDatasetRowProps | IRowProps<IDatasetRowModel>
             </Typography>
           </Grid>
           <Grid item xs={1}>
+          </Grid>
+          <Grid item xs={1}>
+            <ColorPicker className={colorStyle().colorPicker}
+              name='color'
+              value={color}
+              color={"primary"}
+              showPicker={false}
+              onChange={newColor => onDatasetColorChange(newColor)}
+            />
+          </Grid>
+          <Grid item sm={4}>
+            <FormControl fullWidth>
+              <InputLabel id={`${datasetRow.name}ShapeLabel`}>Shape</InputLabel>
+              <Select
+                labelId={`${datasetRow.name}Shape`}
+                fullWidth
+                id={`${datasetRow.name}Shape`}
+                value={shape || 'circle'}
+                autoWidth={true}
+                onChange={onDatasetShapeChange}
+              >
+                {shapes.map(individualShape => (
+                  <MenuItem key={individualShape} value={individualShape}>{individualShape}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={1}>
             <Tooltip title="Show/Hide datasets on graph">
               <IconButton id="ShowHideDataset" onClick={handleHiddenSwitch}>
                 {isHidden ? <VisibilityOffIcon color='primary' /> : <VisibilityIcon color='primary' />}
@@ -60,34 +97,6 @@ export const DatasetRow = (props: IDatasetRowProps | IRowProps<IDatasetRowModel>
                 <DeleteIcon color='primary' />
               </IconButton>
             </Tooltip>
-          </Grid>
-          <Grid item xs={3}>
-            <ColorPicker
-              name='color'
-              InputProps={{ value: color }}
-              label={"Choose Color"}
-              value={color}
-              fullWidth
-              showPicker={false}
-              onChange={newColor => onDatasetColorChange(newColor)}
-            />
-          </Grid>
-          <Grid item sm={4}>
-            <FormControl fullWidth>
-              <InputLabel id={`${datasetRow.name}ShapeLabel`}>{datasetRow.name} Shape</InputLabel>
-              <Select
-                labelId={`${datasetRow.name}Shape`}
-                fullWidth
-                id={`${datasetRow.name}Shape`}
-                value={shape}
-                autoWidth={true}
-                onChange={onDatasetShapeChange}
-              >
-                {shapes.map(individualShape => (
-                  <MenuItem key={individualShape} value={individualShape}>{individualShape}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
           </Grid>
         </Grid>
       </Box>
