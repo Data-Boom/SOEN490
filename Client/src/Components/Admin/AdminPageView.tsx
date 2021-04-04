58a273aae33d25bfecbe23f5f3ef895e7fad48bb
Laith Awad <layth_awad@hotmail.com>
2021-04-04 00:38:25 -0400

    fixed code smells

--- Client/src/Components/Admin/AdminPageView.tsx
+++ Client/src/Components/Admin/AdminPageView.tsx
@@ -1,35 +1,17 @@
-import { AppBar, Container, Snackbar, Tab, Tabs } from "@material-ui/core"
-import React, { useEffect, useState } from 'react'
+import { AppBar, Container, Tab, Tabs } from "@material-ui/core"
 import { TabPanel, a11yProps } from '../Profile/ProfileView'
 
-import { Alert } from '@material-ui/lab'
 import { CategoryManagementTab } from '../Profile/CategoryManagementSection/CategoryManagementTab';
 import { DimensionManagementTab } from '../Profile/UnitManagementSection/DimensionManagementTab'
 import PermissionsTab from '../Profile/PermissionsSection/PermissionsTab'
-import { useUserSelector } from "../../Stores/Slices/UserSlice"
-
-// Tab code taken from: https://material-ui.com/components/tabs/
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-}
+import React from 'react'
 
 export const AdminPageView = () => {
     const [tab, setTab] = React.useState(0)
-    const user = useUserSelector()
-    const [superAdmin, setSuperAdmin] = useState(false)
-    const [alertOpen, setAlertOpen] = useState(true)
 
     const handleChange = (event: React.ChangeEvent<{}>, newTab: number) => {
         setTab(newTab)
     }
-
-    useEffect(() => {
-        if (user.account_permissions == 2) {
-            setSuperAdmin(true)
-        }
-    })
 
     return (
         <>
