273e20bf81f664526f391d4b49dde057456021f4
Rahimuz Zaman <rahimuz_z@yahoo.ca>
2021-04-06 18:57:23 -0400

    #9 updated XLS format download implementation

--- Client/src/Components/DatasetUpload/XLSdatasetDownloadButton.tsx
+++ Client/src/Components/DatasetUpload/XLSdatasetDownloadButton.tsx
@@ -1,18 +1,23 @@
 import { Button } from '@material-ui/core'
 import { IDatasetModel } from '../../Models/Datasets/IDatasetModel'
 import React from 'react'
+import { useDimensionsSelector } from '../../Stores/Slices/DimensionsSlice'
+import { getUnitNameById } from '../../Common/Helpers/DimensionHelpers'
+import * as XLSX from 'xlsx';
 
 interface IProps {
     datasets: IDatasetModel
 }
+
 export const XLSdatasetDownloadButton = (props: IProps) => {
     const { datasets } = { ...props }
+    const dimensions = useDimensionsSelector()
 
     //helper methods
     const variableArray = (): any => {
         var name = " "
         for (var i in datasets.data.variables) {
-            name += datasets.data.variables[i].name + ", " //need to add the variable units, how?
+            name += datasets.data.variables[i].name + " " + getUnitNameById(dimensions, datasets.data.variables[i].unitId) + ", "
         }
         return name
     }
@@ -33,43 +38,40 @@ export const XLSdatasetDownloadButton = (props: IProps) => {
     const dataContentArray = (): any => {
         var name = " "
         for (var i in datasets.data.contents) {
-            name += datasets.data.contents[i].point + " "
+            name += datasets.data.contents[i].point + ", "
         }
         return name
     }
 
-    const downloadedXLSDataDisplayed: string =
-        " Dataset name: " + datasets.dataset_name + "\n " +
-        "Material: " + materialArray() + "\n " +
-        "Publications/Source: \n " +
-        "Authors: " + authorArray() + "\n " +
-        "Title: " + datasets.reference.title + "\n " +
-        "Year: " + datasets.reference.year + "\n " +
-        "Export source: " + " databoom.concordia.ca \n " +
-        "Export date: " + new Date().toLocaleString() + "\n " +
-        "Variables: " + variableArray() + "\n " +
-        "Data: " + dataContentArray() + ", " + datasets.data.dataPointComments + "\n " +
-        "Comments: \n" + datasets.data.comments + " \n "
-
-
+    const downloadedXLSDataDisplayed = [
+        ["Dataset name", datasets.dataset_name],
+        ["Material", materialArray()],
+        ["Data type", datasets.data_type],
+        ["Category", datasets.category],
+        ["Subcategory ", datasets.subcategory],
+        ["Publication/sources"],
+        ["Authors", authorArray()],
+        ["Title ", datasets.reference.title],
+        ["Type", datasets.reference.type],
+        ["Publisher", datasets.reference.publisher],
+        ["Volume", datasets.reference.volume],
+        ["Pages ", datasets.reference.pages],
+        ["DOI ", datasets.reference.doi],
+        ["Issue ", datasets.reference.issue],
+        ["Year ", datasets.reference.year],
+        ["Export source ", "databoom.concordia.ca"],
+        ["Export date ", new Date().toLocaleString()],
+        [" ", variableArray(), "comments"],
+        [" ", dataContentArray(), datasets.data.dataPointComments]
+    ]
     const handleXLSDownload = () => {
-        download("xlsdataset.xls", downloadedXLSDataDisplayed)
+        const fileName = "xlsdataset.xls"
+        const worksheet = XLSX.utils.aoa_to_sheet(downloadedXLSDataDisplayed)
+        const workbook = XLSX.utils.book_new()
+        XLSX.utils.book_append_sheet(workbook, worksheet, "downloadedXLSDataDisplayed")
+
+        XLSX.writeFile(workbook, fileName)
     }
-
-    //stolen from https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
-    function download(filename: string, text: string) {
-        const element = document.createElement('a')
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
-        element.setAttribute('download', filename)
-
-        element.style.display = 'none'
-        document.body.appendChild(element)
-
-        element.click()
-
-        document.body.removeChild(element)
-    }
-
     return (
         <Button id="download-xls" onClick={handleXLSDownload} color="primary" variant="contained"> XLS </Button>
     )
