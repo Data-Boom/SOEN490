e05f310dacbb6db9c838c4f4a9293accefa98e62
vatika <vatikaprasad@gmail.com>
2021-04-11 23:23:44 -0400

    fixed code smell

--- Client/src/Components/DatasetUpload/DatasetForm/DataSection/DatasetDataTable.tsx
+++ Client/src/Components/DatasetUpload/DatasetForm/DataSection/DatasetDataTable.tsx
@@ -126,14 +126,6 @@ export const DatasetDataTable = (props: IProps): any => {
     return data.contents.findIndex(suchRow => suchRow.point == row)
   }
 
-  const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-
-    return result;
-  };
-
   const renderTopButtons = (): any => {
     return (
       <>
